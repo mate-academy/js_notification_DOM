8164d77e25a25a3f07ba72cce8d628808c801f89
Ihor Hranetskyi <graniecki20@gmail.com>
2020-07-10 21:48:39 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const body = document.body;
+
+  body.insertAdjacentHTML('afterbegin', `
+  <div class='notification ${type}'>
+  <h2 class='title'>${title}</h2>
+  <p>${description}</p>
+  </div>
+  `);
+
+  const notification = document.querySelector(`.notification`);
+
+  notification.style.cssText = `top: ${top}px; right: ${right}px`;
+
+  window.setTimeout(() => {
+    notification.remove();
+  }, 2000);
+};
+
+pushNotification(100, 500, 'Title', 'Blablabla', 'warning');
