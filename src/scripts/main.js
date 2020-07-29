8cf787db6b978141f3457bd20db0e5a1f6aac0ce
Alex Arabov <arabov@ukr.net>
2020-07-29 12:00:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const doc = document.querySelector('body');
+
+  const notificationElementDiv = document.createElement('div');
+  const notificationElementH2 = document.createElement('h2');
+  const notificationElementP = document.createElement('p');
+
+  notificationElementDiv.classList.add('notification', type);
+  notificationElementH2.classList.add('title');
+  notificationElementDiv.style.cssText = `top: ${top}px; right: ${right}px`;
+
+  notificationElementH2.innerText = title;
+  notificationElementP.innerText = description;
+
+  notificationElementDiv.append(notificationElementH2, notificationElementP);
+  doc.append(notificationElementDiv);
+
+  setTimeout(function() {
+    notificationElementDiv.remove();
+  }, 2000);
+};
+
+pushNotification(10, 15, 'Error', 'This is example of result ', 'error');
