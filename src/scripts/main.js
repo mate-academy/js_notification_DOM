e972067578c6dad8ea8bfc1fcdf1eea80367a3db
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-14 12:41:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const createDiv = document.createElement('div');
+  const titleH2 = document.createElement('h2');
+  const createP = document.createElement('p');
+
+  createDiv.classList.add('notification', `${type}`);
+
+  createDiv.style.cssText = `top: ${top}px; right: ${right}px`;
+
+  titleH2.classList.add('title');
+  titleH2.textContent = `${title}`;
+
+  createP.textContent = `${description}`;
+
+  createDiv.append(titleH2, createP);
+
+  document.querySelector('body').append(createDiv);
+
+  setTimeout(() => createDiv.remove(), 2000);
+};
+
+pushNotification(50, 50,
+  'TITLE', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  'success');
