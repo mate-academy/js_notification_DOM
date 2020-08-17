32e6d5d3188bba3054876cc528a2d9cff7d74f92
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-17 18:41:25 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  document.body.insertAdjacentHTML('beforeend',
+    `<div
+      class="notification ${type}"
+      style="position: absolute; top: ${top}; right: ${right}"
+    >
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>`);
+};
+
+setTimeout(() => {
+  pushNotification('100px', '100px', 'Поздравляем!',
+    `Вы стали 228-ым посетителем нашего сайта.
+      <br><br> Нажмите здесь чтобы ввести данные
+      своей банковской карты и лишиться денег!`,
+    'warning');
+}, 2000);
