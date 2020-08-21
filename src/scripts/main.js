285f63f63ff9381d0e8acd4bc09e5555246b55f9
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-21 14:44:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const body = document.querySelector('body');
+
+  setTimeout(() => body.insertAdjacentHTML('afterbegin', `
+  <div class="notification ${type}"
+    style="position: absolute; top: ${top}; right: ${right}">
+    <h2 class="title">
+      ${title}
+    </h2>
+    <p class="description">
+      ${description}
+    </p>
+
+  </div>
+  `), 2000);
+};
+
+pushNotification('50px', '50px', 'Попробуйте снова!',
+  'Ваши данные введены некорректно, попробуйте, пожалуйста, снова!', 'error');
