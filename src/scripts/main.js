992cda5d530c9834ea54fb0a90d908c672b5d4fc
mpone <m.parphenjuk@gmail.com>
2020-08-24 22:45:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+
+  message.classList.add('notification', type);
+
+  message.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+  message.style.top = `${top}px`;
+  message.style.right = `${right}px`;
+  document.body.append(message);
+  setTimeout(() => message.remove(), 2000);
+};
+
+pushNotification(
+  30,
+  30,
+  'Congratulations!',
+  'You won a prize!',
+  'success');
