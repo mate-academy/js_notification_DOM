a9a51c465c6ae02873a23468479812bf8f42b566
Mikle25 <mikle2593@gmail.com>
2020-08-24 18:35:03 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification');
+  notification.classList.add(type);
+  notification.style.right = right;
+  notification.style.top = top;
+
+  notification.innerHTML = (
+    `<h2 class='title'>${title}</h2>
+    <p>${description}</p>`
+  );
+
+  document.body.append(notification);
+
+  setTimeout(() => notification.remove(), 2000);
+};
+
+pushNotification('50px', '50px', 'Title',
+  `This is example of result.
+  Notification should contain title and description`,
+  'warning');
