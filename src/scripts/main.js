4c1ba317a4118a54c03bc16b72fe4934b2009a7b
Elena-brv <leena.brv@gmail.com>
2020-08-24 17:17:49 +0300

    notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+
+  message.className = 'notification';
+  message.classList.add(type);
+
+  message.innerHTML = `
+    <h2>${title}</h2>
+    <p>${description}</p>
+  `;
+
+  message.style.top = top;
+  message.style.right = right;
+
+  setTimeout(() => {
+    message.remove();
+  }, 2000);
+
+  document.querySelector('body').children[0].after(message);
+};
+
+pushNotification(
+  '15px',
+  '15px',
+  'Title',
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae ea illum!',
+  'warning'
+);
