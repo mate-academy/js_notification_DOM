51329f18d09ddb593cfac3d2e56247b561861b4c
DmJSCourse <dimonua@gmail.com>
2020-08-25 16:53:50 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notification = document.createElement('div');
+  const notificationHeader = document.createElement('h2');
+  const notificationParagraph = document.createElement('p');
+
+  notification.classList.add('notification');
+  notification.classList.add(type);
+  notificationHeader.classList.add('title');
+
+  notificationHeader.innerText = title;
+  notificationParagraph.innerText = description;
+
+  notification.appendChild(notificationHeader);
+  notification.appendChild(notificationParagraph);
+
+  notification.style.position = 'absolute';
+  notification.style.top = top;
+  notification.style.right = right;
+
+  document.body.appendChild(notification);
+
+  window.setTimeout(deleteNotification, 2000);
+};
+
+function deleteNotification() {
+  const toDelete = document.querySelector('.notification');
+
+  toDelete.remove();
+}
+
+pushNotification('5px', '5px', 'Title', 'this is a description', 'alert');
