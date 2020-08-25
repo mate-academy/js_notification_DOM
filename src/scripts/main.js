bf44fdb3b88a0ce2816dd715a0d2ca4533e2e711
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-25 16:57:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const pushNotification = (top, right, title, description, type) => {
   const body = document.querySelector('body');
 
-  setTimeout(() => body.insertAdjacentHTML('afterbegin', `
+  body.insertAdjacentHTML('afterbegin', `
   <div class="notification ${type}"
     style="position: absolute; top: ${top}; right: ${right}">
     <h2 class="title">
@@ -14,8 +14,10 @@ const pushNotification = (top, right, title, description, type) => {
     </p>
 
   </div>
-  `), 2000);
+  `);
 };
+
+setTimeout(() => document.querySelector('.notification').remove(), 2000);
 
 pushNotification('50px', '50px', 'Попробуйте снова!',
   'Ваши данные введены некорректно, попробуйте, пожалуйста, снова!', 'error');
