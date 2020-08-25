330079f75c17009f623f33ded42a587c28d1ccc8
oggyrock <steelo.sun@gmail.com>
2020-08-25 17:01:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notification = document.createElement('div');
+
+  document.body.prepend(notification);
+
+  notification.className = `notification ${type}`;
+  notification.style.position = 'absolute';
+  notification.style.top = `${top}px`;
+  notification.style.right = `${right}px`;
+
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.textContent = title;
+  notificationTitle.className = 'title';
+  notification.append(notificationTitle);
+
+  const notificationDescription = document.createElement('p');
+
+  notificationDescription.textContent = description;
+  notification.append(notificationDescription);
+};
+
+const removeNotification = () => {
+  document.querySelector('.notification').remove();
+};
+
+pushNotification(100, 100, 'title', 'description', 'warning');
+setTimeout(removeNotification, 2000);
