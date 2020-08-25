4a798659497c2a746109705d84d4579ad3ae76ee
Artur <artroyal.pro@gmail.com>
2020-08-25 23:38:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const body = document.querySelector('body');
+
+  const message = `
+    <div
+      class="notification ${type}"
+      <style="position: absolute; top: ${top}; right: ${right}">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `;
+
+  body.insertAdjacentHTML('afterbegin', message);
+
+  const notification = document.querySelector('.notification');
+
+  setTimeout(() => notification.remove(), 2000);
+};
+
+pushNotification('10px', '10px', 'FATAL ERROR', 'Data not received', 'error');
