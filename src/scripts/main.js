641d62b712209837b4335d9b3f3798fdbc53406b
Alisa <cheshyr.alisa@gmail.com>
2020-08-25 15:48:51 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const body = document.body;
+
+  body.insertAdjacentHTML('afterbegin', `
+    <div
+      class="notification ${type}"
+      style="position: absolute; top: ${top}; right: ${right}">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `);
+
+  const notification = document.querySelector('.notification');
+
+  setTimeout(() => notification.remove(), 2000);
+};
+
+pushNotification('10px', '10px',
+  'qweqweqweqwe', 'qweqweqweqweqweqweqweqwe', 'success');
