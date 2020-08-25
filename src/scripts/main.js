956ebd3b6f7fa04a3a09b6fd2a5e57a5a2ff97c3
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-25 17:47:22 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  document.body.insertAdjacentHTML('beforeend',
+    `<div
+      class="notification ${type}"
+      style="position: absolute; top: ${top}; right: ${right}"
+    >
+
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>`);
+};
+
+setTimeout(() => {
+  pushNotification('20px', '20px', 'Hi!', 'Have a nice day:)', 'success');
+}, 2000);
