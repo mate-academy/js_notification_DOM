f807ca22854124ebaed89baad4b4043d8407b14a
Anton <tsymbal044@gmail.com>
2020-08-25 18:21:18 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,25 @@
 // const pushNotification = (top, right, title, description, type) => {
 //   // write code here
 // };
+
+const pushNotification = (top, right, title, description, type) => {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div
+      class="notification ${type}"
+      style="position: absolute; top: ${top}; right: ${right}"
+    >
+      <h2 class="${title}">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `);
+};
+
+setTimeout(() => {
+  pushNotification(
+    '50px',
+    '50px',
+    'ВНИМАНИЕ!!!',
+    'Спасибо за внимание!',
+    'warning'
+  );
+}, 2000);
