35330e1c9ccaad6b65a5cd418b4bef0a15d4aeb1
Kostya Sabada <kostyasabada@gmail.com>
2020-08-25 19:25:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+
+  message.classList = `notification ${type}`;
+
+  message.innerHTML = `
+  <h2 class="title">${title}</h2>
+  <p>${description}</p>
+`;
+  message.style.top = `${top}px`;
+  message.style.right = `${right}px`;
+  document.body.append(message);
+
+  setTimeout(() => message.remove(), 2000);
+};
+
+pushNotification(30, 20, `Hello!`,
+  `This is example of result.
+  Notification should contain title and description`,
+  'error');
