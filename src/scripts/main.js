5d426d829f091428c695557d93a15914beb5107b
unknown <pavlo.com.5@gmail.com>
2020-08-26 15:23:53 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,24 @@
 'use strict';
 
 const pushNotification = (top, right, title, description, type) => {
+  const topPos = top + 'px';
+  const rightPos = right + 'px';
+
   document.body.insertAdjacentHTML('beforeend', `
-    <div class="notification ${type}" style="top: ${top}; right: ${right};">
-      <h2>${title}</h2>
+    <div
+      class="notification ${type}"
+      style="top: ${topPos}; right: ${rightPos};"
+    >
+      <h2 class="title">${title}</h2>
       <p>${description}</p>
     </div>
   `);
 
-  const message = document.body.lastElementChild;
+  const message = document.querySelector('.notification');
 
   setTimeout(() => {
     message.remove();
   }, 2000);
 };
 
-pushNotification(100, 100, 'Warning!', 'Be careful!!!', 'warning');
+pushNotification(10, 10, 'Warning!', 'Be careful!!!', 'warning');
