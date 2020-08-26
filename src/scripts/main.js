3a77bfd7f485290cbbb31b6378d66c2e85b953db
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-08-26 12:07:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const body = document.querySelector('body');
+let div;
+let head;
+let text;
+
+const pushNotification = (top, right, title, description, type) => {
+  div = body.insertAdjacentElement('afterbegin', document.createElement('div'));
+  div.classList.add('notification', type);
+
+  div.style.top = top;
+  div.style.right = right;
+
+  head = div.insertAdjacentElement('afterbegin', document.createElement('h2'));
+  head.classList.add('tittle');
+  head.innerText = title;
+
+  text = head.insertAdjacentElement('afterend', document.createElement('p'));
+  text.innerText = description;
+
+  setTimeout(() => {
+    div.remove();
+  }, 2000);
+};
+
+setTimeout(
+  pushNotification,
+  300,
+  10,
+  10,
+  'Notification',
+  'You have one message',
+  'error'
+);
