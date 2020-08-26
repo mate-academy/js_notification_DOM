c6b147b837e168701f37cfe7809e3133e8d855de
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-26 23:02:01 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,22 @@
 const pushNotification = (top, right, title, description, type) => {
   const notification = document.createElement('div');
 
-  notification.className = `notification`;
-  notification.classList += ` ${type}`;
-
-  notification.innerHTML = `
-    <h2 class="title">${title}</h2>
-    <p>${description}</p>
-  `;
-
-  notification.style.top = `${top}%`;
-  notification.style.right = `${right}%`;
+  notification.insertAdjacentHTML('afterbegin', `
+  <div class='notification ${type}' style='top=${top} right=${right}'>
+    <h2 class='title'>
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  </div>
+`);
 
   document.body.append(notification);
   setTimeout(() => notification.remove(), 2000);
 };
 
-pushNotification(5, 2, `Hello!`,
+pushNotification('5%', '2%', `Hello!`,
   `This is example of result.
   Notification should contain title and description`,
   'error');
