f178f44f805e035137e402720aca2cb608dfcda7
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-08-26 11:17:43 +0300

    added features that removed message after 2s

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,22 +2,21 @@
 
 const pushNotification = (top, right, title, description, type) => {
   document.body.insertAdjacentHTML('afterbegin',
-    `
-    <div
+    `<div
       class="notification ${type}"
       style="position: absolute; top: ${top}; right: ${right}"
     >
       <h2 class="title">${title}</h2>
       <p>${description}</p>
-    </div>
-    `);
+    </div>`);
 };
 
-setTimeout(() => {
-  pushNotification('50px', '25px', 'Поздравляем!!!',
-    `Вы были избраны рандомным путем и стали обладателем
-    автомобиля Toyota Land Cruiser.
-    <br><br>
-    Как можно быстрее переходите по ссылке и получите ваш подарок!`,
-    'success');
-}, 2000);
+setTimeout(() => document.querySelector('div').remove(), 2000);
+
+pushNotification('50px', '25px', 'Поздравляем!!!'
+  , `Вы были избраны рандомным путем и стали обладателем
+  автомобиля Toyota Land Cruiser.
+  <br><br>
+  Как можно быстрее переходите по ссылке и получите ваш подарок!`
+  , 'success'
+);
