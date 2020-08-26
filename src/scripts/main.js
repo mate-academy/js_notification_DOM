454143e1c33b0c22ae911e7852336f569c59b9ce
oivannikov <alex0502ivannikov@gmail.com>
2020-08-26 11:05:59 +0300

    refactored the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,22 +5,25 @@ const pushNotification = (top, right, title, description, type) => {
   const h2 = document.createElement('h2');
   const p = document.createElement('p');
 
-  div.className = (`notification ${type}`);
+  div.className = `notification ${type}`;
   div.style.cssText = `top: ${top}; right: ${right};`;
   div.append(h2);
   div.append(p);
-  h2.className = ('title');
-  h2.textContent = `${title}`;
-  p.textContent = `${description}`;
+  h2.className = 'title';
+  h2.textContent = title;
+  p.textContent = description;
   document.body.append(div);
+
+  setTimeout(() => {
+    div.remove();
+  }, 2000);
 };
 
-setTimeout(() => {
-  pushNotification(
-    '50px',
-    '50px',
-    'Вы выиграли анекдот!',
-    `Весь день искал дома сосиски, но так и не нашел.
-    Потом посмотрел на довольное лицо тестя и понял: сосиски в тесте.`,
-    'success');
-}, 2000);
+pushNotification(
+  '50px',
+  '50px',
+  'Вы выиграли анекдот!',
+  `Весь день искал дома сосиски, но так и не нашел.
+  Потом посмотрел на довольное лицо тестя и понял: сосиски в тесте.`,
+  'success'
+);
