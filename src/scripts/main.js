f77edc7c03ceea7494a2aeb51f3bdbf38a5d9f7a
Artemii Kabanov <artemiikab@gmail.com>
2020-08-26 17:11:00 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const pushNotification = (top, right, title, description, type) => {
+  document.body.insertAdjacentHTML(`beforeend`,
+    `<div
+      class = 'notification ${type}'
+      style = 'position: absolute; top: ${top}; right: ${right}'
+    >
+      <h2 class = 'title'> ${title} </h2>
+      <p> ${description} </p>
+    </div>`);
+
   setTimeout(() => {
-    document.body.insertAdjacentHTML(`beforeend`,
-      `<div
-        class = 'notification ${type}'
-        style = 'position: absolute; top: ${top}; right: ${right}'
-      >
-        <h2 class = 'title'> ${title} </h2>
-        <p> ${description} </p>
-      </div>`);
+    document.querySelector('.notification').remove();
   }, 2000);
 };
 
