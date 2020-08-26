01a6f85d5f7aa5a112f80abd42056954882eba76
ChrisZinch <chrispakulyak@gmail.com>
2020-08-26 10:51:53 +0300

    success

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+
+  document.body.append(message);
+
+  message.className = `notification ${type}`;
+
+  message.innerHTML = `
+    <h2 class='title'>${title}<h2>
+    <p>${description}<p>
+  `;
+
+  message.style.top = `${top}px`;
+  message.style.right = `${right}px`;
+
+  setTimeout(() => message.remove(), 2000);
+};
+
+pushNotification(20, 40, 'title', 'Great! Good job!', 'success');
