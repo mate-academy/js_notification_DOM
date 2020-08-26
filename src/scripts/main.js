76ec681b68bbc2abe572578be8a86c2c8303b2fd
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-26 13:02:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+
+  notification.insertAdjacentHTML('afterbegin', `
+    <h2 class='title'>${title}</h2>
+    <p>${description}</p>
+  `);
+
+  notification.top = `${top}px`;
+  notification.right = `${right}px`;
+
+  document.body.append(notification);
+
+  setTimeout(() => notification.remove(), 3000);
+};
+
+const customDescription = 'Notification will disappear in a few seconds';
+
+pushNotification(50, 50, 'Don\'t touch anything', customDescription, 'warning');
