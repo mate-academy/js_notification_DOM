dce8d3ff7d9bbc8e90cd36b94fdc1cb6ce1edeb5
nznmvs <nznmvs@gmail.com>
2020-08-26 09:37:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  document.body.insertAdjacentHTML('beforeend',
+    `<div
+      class="notification ${type}"
+      style="position: absolute; top: ${top}; right: ${right}"
+    >
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+      </div>
+    `);
+
+  setTimeout(() => document.querySelector('.notification').remove(), 2000);
+};
+
+pushNotification('10px', '10px', 'Title',
+  `This is example of result.
+  Notification should contain title and description`,
+  'error');
