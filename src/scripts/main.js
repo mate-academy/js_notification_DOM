b496578fbf11ca2de8ff7f24969f85c7016d5b53
Alona <aionovaionova@gmail.com>
2020-10-07 13:31:43 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const note = document.createElement('div');
+
+  note.style.top = top + 'px';
+  note.style.right = right + 'px';
+  note.classList.add(type);
+  note.classList.add('notification');
+
+  const noteTitle = document.createElement('h2');
+
+  noteTitle.innerText = title;
+
+  const noteText = document.createElement('p');
+
+  noteText.innerText = description;
+  document.body.append(note);
+  note.append(noteTitle);
+  note.append(noteText);
+  setTimeout(() => note.remove(), 2000);
+};
+
+pushNotification(20, 100, 'Success alert', 'Everything is ok', 'success');
