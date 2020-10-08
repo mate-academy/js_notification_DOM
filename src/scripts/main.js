01ac4a5df7c94b441a942cf4d578d3390fc69245
Yazheviks <vicsergeyeva@gmail.com>
2020-10-08 13:48:26 +0300

    innerHTML

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,20 +2,15 @@
 
 function pushNotification(top, right, title, description, type) {
   const notification = document.createElement('div');
-  const titleOfMessage = document.createElement('h2');
-  const descriptionOfMessage = document.createElement('p');
 
+  notification.innerHTML = `
+      <h2>${title}</h2>
+      <p>${description}</p>
+  `;
+
+  document.body.append(notification);
   notification.classList.add('notification');
   notification.classList.add(`${type}`);
-  titleOfMessage.innerText = title;
-  descriptionOfMessage.innerText = description;
-
-  notification.style.top = top;
-  notification.style.right = right;
-
-  notification.append(titleOfMessage);
-  notification.append(descriptionOfMessage);
-  document.body.append(notification);
 
   window.setTimeout(() => notification.remove(), 2000);
 }
