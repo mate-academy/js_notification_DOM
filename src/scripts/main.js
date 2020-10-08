42c46c704760ccb3240fccec31ad9b1f3ffec20a
Petro <petdavy@1991gmail.com>
2020-10-08 23:03:13 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,42 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageDescription = document.createElement('p');
+
+  message.className = `notification ${type}`;
+  message.style.top = `${top}px`;
+  message.style.right = `${right}px`;
+
+  messageTitle.className = 'title';
+  messageTitle.textContent = title;
+
+  messageDescription.textContent = description;
+
+  message.append(messageTitle);
+  message.append(messageDescription);
+  document.body.append(message);
+
+  setTimeout(() => message.remove(), 2000);
+};
+
+pushNotification(
+  10,
+  10,
+  'Don\'t press MA logo!',
+  'If you are really curious you can press it. But I warned you.',
+  'warning'
+);
+
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', () => {
+  pushNotification(
+    10,
+    10,
+    'I told you DON\'T PRESS!!!',
+    'People never listen to me. This logo doesn\'t do anything here.',
+    'error'
+  );
+});
