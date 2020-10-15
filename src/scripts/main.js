b7dc919882678bc5efa2f517e66b9acdadf3316d
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-10-15 10:54:58 +0300

    Solution fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,23 @@
 const pushNotification = (top, right, title, description, type) => {
   const message = document.createElement('div');
 
-  message.className = 'notification';
-  message.classList.add(type);
+  message.classList.add('notification', type);
   message.style.top = `${top}px`;
   message.style.right = `${right}px`;
   document.querySelector('body').append(message);
 
   message.innerHTML = `
-  <h2 class="title">${title}</h2>
-  <p>${description}</p>
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
   `;
 
   setTimeout(() => message.remove(), 2000);
 };
 
-pushNotification(10, 10, 'Title of notification',
-  'Some interesting thing in this notification', 'success');
+pushNotification(
+  10,
+  10,
+  'Title of notification',
+  'Some interesting thing in this notification',
+  'success'
+);
