8a7cd3519d6ad3abee1240e45884269ba8984bb6
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-15 18:16:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notification = document.createElement('div');
+
+  notification.className = 'notification';
+  document.body.append(notification);
+
+  notification.style.top = `${top}px`;
+  notification.style.right = `${right}px`;
+
+  notification.innerHTML = `
+    <h2>
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  `;
+
+  notification.classList.add(type);
+
+  setTimeout(() => {
+    notification.style.cssText += 'opacity: 0; transition: opacity 1s';
+  }, 2000);
+};
+
+pushNotification(
+  20,
+  20,
+  'Title',
+  'Meow meow meow',
+  'success'
+);
