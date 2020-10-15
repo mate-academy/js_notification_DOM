5739ba380eed46cc8fa7d326dae5cbd85026826d
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-15 17:01:43 +0300

    solution notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const block = document.createElement('div');
+  const blockTitle = document.createElement('h2');
+  const blockText = document.createElement('p');
+  const body = document.body;
+
+  block.style.top = `${top}px`;
+  block.style.right = `${right}px`;
+  block.classList.add('notification');
+
+  blockTitle.textContent = title;
+  blockText.textContent = description;
+
+  block.append(blockTitle, blockText);
+  body.append(block);
+
+  setTimeout(() => block.remove(), 2000);
+};
+
+pushNotification(30, 750, 'Operation success', 'was added one element', 'text');
