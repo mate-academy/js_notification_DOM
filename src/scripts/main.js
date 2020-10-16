6b32cf0fbb34dbc9b5a7ae2bddcb576691d5e2d0
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-16 21:02:39 +0300

    timer setted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,40 @@
 'use strict';
 
-const pushNotification = (top, right, title, description, type) => {
+const pushNotification = (top, right, title, description, type = 'warning') => {
   const body = document.body;
   const message = document.createElement('div');
   const messageTitle = document.createElement('h2');
   const messageDescription = document.createElement('p');
+  const showWhile = 2000;
+  const showafter = 1000;
 
-  body.append(message);
-  message.append(messageTitle);
-  message.append(messageDescription);
+  const displayContent = () => {
+    body.append(message);
+    message.append(messageTitle);
+    message.append(messageDescription);
+  };
 
+  setTimeout(() => displayContent(), showafter);
+
+  setTimeout(() => {
+    message.style.display = 'none';
+  }, showafter + showWhile);
+
+  message.classList.add('.notification');
   message.style.position = 'absolute';
   message.style.maxWidth = '230px';
   message.style.top = top;
   message.style.right = right;
-  message.style.backgroundColor = '#F4E5BA';
   message.style.padding = '15px';
   message.style.borderRadius = '10px';
 
+  type === 'success'
+    ? message.style.backgroundColor = '#8BE78C'
+    : type === 'error'
+      ? message.style.backgroundColor = '#F8655E'
+      : message.style.backgroundColor = '#F4E5BA';
+
+  messageTitle.classList.add('.title');
   messageTitle.style.margin = '0';
   messageTitle.style.fontSize = '16px';
   messageTitle.textContent = title;
@@ -32,5 +49,5 @@ pushNotification('10px',
   '10px',
   'Title',
   `This is example of result. 
-  Notofication should contain title and description.`
-);
+  Notofication should contain title and description.`,
+  'success');
