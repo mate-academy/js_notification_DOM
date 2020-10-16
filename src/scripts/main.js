bcaef853d73e7d400a164a9113b4a9d5bae1110a
Yuriy Putsan <putsan57@gmail.com>
2020-10-16 23:13:12 +0300

    create notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notify = document.createElement('div');
+
+  notify.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+
+  notify.classList.add('notification', type);
+  notify.style.top = top;
+  notify.style.right = right;
+
+  document.body.append(notify);
+  setTimeout(() => notify.remove(), 2000);
+};
+
+pushNotification(
+  '20px',
+  '10px',
+  'You are awesome',
+  'Remember it!',
+  'success'
+);
