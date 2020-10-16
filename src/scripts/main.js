4bf8eb454d7c4f44e9d292606c80328a82deee45
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-16 19:12:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const body = document.body;
+  const message = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageDescription = document.createElement('p');
+
+  body.append(message);
+  message.append(messageTitle);
+  message.append(messageDescription);
+
+  message.style.position = 'absolute';
+  message.style.maxWidth = '230px';
+  message.style.top = top;
+  message.style.right = right;
+  message.style.backgroundColor = '#F4E5BA';
+  message.style.padding = '15px';
+  message.style.borderRadius = '10px';
+
+  messageTitle.style.margin = '0';
+  messageTitle.style.fontSize = '16px';
+  messageTitle.textContent = title;
+
+  messageDescription.textContent = description;
+  messageDescription.style.fontSize = '14px';
+  messageDescription.style.margin = '15px 0 0 0';
+  messageDescription.style.lineHeight = '1.3rem';
+};
+
+pushNotification('10px',
+  '10px',
+  'Title',
+  `This is example of result. 
+  Notofication should contain title and description.`
+);
