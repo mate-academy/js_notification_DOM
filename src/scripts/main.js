887d6ff666c14238b68f27ca30d32c31915a5354
Evgeniy <more.inspirationn@gmail.com>
2020-10-16 14:06:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+
+  message.className = `notification ${type}`;
+  message.style.top = `${top}px`;
+  message.style.right = `${right}px`;
+
+  message.innerHTML = `
+    <h2>${title}</h2>
+    <p>${description}</p>
+  `;
+  document.body.append(message);
+
+  setTimeout(() => message.remove(), 2000);
+};
+
+pushNotification(
+  '10',
+  '10',
+  'Remember!',
+  'The winter is coming :`(',
+  'warning'
+);
