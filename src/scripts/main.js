f9d7fe2fcc8784f63cf934755aea8c8ef7b79ac5
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-17 17:25:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageText = document.createElement('p');
+
+  message.className = `notification ${type}`;
+  message.style.top = top;
+  message.style.right = right;
+
+  messageTitle.className = 'title';
+  messageTitle.textContent = title;
+
+  messageText.textContent = description;
+
+  message.append(messageTitle);
+  message.append(messageText);
+  document.body.append(message);
+
+  setTimeout(() => message.remove(), 2000);
+};
+
+pushNotification(10, 10, 'Congratulations!', 'Everything is right', 'success');
