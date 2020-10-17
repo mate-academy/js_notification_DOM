5f3e1ec0919a7f2dd0e121730d78e5ce7aa3bba7
Liza <elizabethhoncharenko@gmail.com>
2020-10-17 14:22:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const message = document.createElement('div');
+
+  message.classList.add('notification', type);
+
+  message.style.top = `${top}px`;
+  message.style.right = `${right}px`;
+
+  const heading = document.createElement('h2');
+
+  heading.classList.add('title');
+  heading.textContent = title;
+
+  const desc = document.createElement('p');
+
+  desc.textContent = description;
+
+  message.append(heading);
+  message.append(desc);
+  document.body.append(message);
+
+  setTimeout(() => (message.hidden = true), 2000);
+};
+
+pushNotification(10, 10, 'error', 'lorem ispum ...', 'error');
