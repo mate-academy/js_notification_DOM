0c9b00a4ff8822e50320421abb753b6727ecc912
dmitry <akulinind@gmail.com>
2020-10-17 11:52:56 +0300

    add notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notification = document.createElement('div');
+  const heading = document.createElement('h1');
+  const content = document.createElement('p');
+  const body = document.querySelector('body');
+
+  heading.textContent = title;
+  content.textContent = description;
+  notification.classList.add('notification');
+  notification.classList.add(type);
+  notification.append(heading);
+  notification.append(content);
+  notification.style.right = right + 'px';
+  notification.style.top = top + 'px';
+  body.append(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
+};
+
+pushNotification(100, 100, 'TIT', 'TEXT', 'error');
