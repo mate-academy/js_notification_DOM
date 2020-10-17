2c3bc3a4b47fc853ab7c227671104c247d6be51f
Eugene <evolokhin@gmail.com>
2020-10-17 11:00:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const body = document.querySelector('body');
+
+  body.innerHTML = `
+    <div>
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+    <h1 class="logo">MA</h1>
+    <script type="text/javascript" src="scripts/main.js"></script>
+  `;
+
+  const notification = document.querySelector('div');
+
+  notification.classList.add('notification');
+  notification.classList.add(`${type}`);
+  notification.style.top = `${top}px`;
+  notification.style.right = `${right}px`;
+
+  setTimeout(() => notification.remove(), 3000);
+};
+
+pushNotification(20, 20, 'HEY COWBOY!!!', "Let's ROCK!", 'success');
