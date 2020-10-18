35b1b89a9911b6b8a7f27289d7a7ef05075ce451
KirillLutsenko <mnrovski68@gmail.com>
2020-10-18 18:38:42 +0300

    Creating notification using JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const errorMessage = document.createElement('div');
+  const errorMessageTitle = document.createElement('h2');
+  const errorMessageDescription = document.createElement('p');
+
+  errorMessage.className = `notification ${type}`;
+  errorMessage.style.top = `${top}px`;
+  errorMessage.style.right = `${right}px`;
+
+  errorMessageTitle.className = 'title';
+  errorMessageTitle.textContent = title;
+
+  errorMessageDescription.textContent = description;
+
+  errorMessage.append(errorMessageTitle);
+  errorMessage.append(errorMessageDescription);
+  document.body.append(errorMessage);
+
+  setTimeout(() => errorMessage.remove(), 2000);
+};
+
+pushNotification(
+  10,
+  20,
+  'Attention!',
+  `Did you repeat the theory exactly before the lecture?`,
+  'error'
+);
