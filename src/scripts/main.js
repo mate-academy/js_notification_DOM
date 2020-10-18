b7cfc629b0e492a45c4dc716846aad8361f030f1
38063 <xeilen1112@gmail.com>
2020-10-18 15:14:16 +0300

    -solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const alertBlock = document.createElement('div');
+  const alertTitle = document.createElement('h2');
+  const alertDescription = document.createElement('p');
+
+  document.body.appendChild(alertBlock);
+  alertBlock.classList.add(`notification`);
+  alertBlock.classList.add(type);
+
+  alertBlock.append(alertTitle);
+  alertBlock.append(alertDescription);
+
+  alertTitle.classList.add('title');
+  alertTitle.innerText = title;
+
+  alertDescription.innerText = description;
+
+  alertBlock.style.top = `${top}px`;
+  alertBlock.style.right = `${right}px`;
+
+  setTimeout(() => alertBlock.remove(), 2000);
+};
+
+pushNotification(
+  50,
+  50,
+  'Success!',
+  'Success message',
+  'success'
+);
