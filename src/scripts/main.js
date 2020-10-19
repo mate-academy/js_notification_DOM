457839b8eb08591bc25e18100ee47d53e9de1910
Atteroot <yuora556@gmail.com>
2020-10-19 15:04:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const block = document.createElement('div');
+
+  block.className = 'notification';
+  document.body.append(block);
+
+  block.style.top = `${top}px`;
+  block.style.right = `${right}px`;
+  block.classList.add(type);
+
+  block.innerHTML = `
+  <h2>
+    ${title}
+  </h2>
+  <p>
+    ${description}
+  </p>
+  `;
+
+  setTimeout(() => {
+    block.style.cssText += 'opacity: 0; transition: opacity 300ms';
+  }, 2000);
+};
+
+pushNotification(20, 20, 'Hey, Mate!', 'You here forever', 'success');
