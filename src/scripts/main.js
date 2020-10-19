0522cbfea516a9d9f2d92a7f5c8b7b3b3793431e
Yurii <yurastt1@gmail.com>
2020-10-19 18:27:34 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const body = document.querySelector('body');
+
+const pushNotification = (top, right, title, description, type) => {
+  body.insertAdjacentHTML('afterbegin', `
+    <div class='${type} notification'>
+      <h2 class='title'>
+        ${title}
+      </h2>
+      <p>
+        ${description}
+      </p>
+    </div>
+  `);
+
+  const notification = document.querySelector('.notification');
+
+  notification.style.cssText = `top: ${top}px; right: ${right}px`;
+
+  setTimeout(() => notification.remove(), 2000);
+};
+
+pushNotification(10, 10, 'Oh no!', 'you are in trouble', 'error');
