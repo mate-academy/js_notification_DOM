0f3f9b0432380e3bd5bbec6fbc2e70fc77cc40a3
Vitalik <ztx25sss@gmail.com>
2020-10-19 14:18:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,42 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const block = document.createElement('div');
+
+  block.classList.add('notification', type);
+
+  block.style.top = `${top}px`;
+  block.style.right = `${right}px`;
+
+  block.insertAdjacentHTML(
+    'beforeend', `<h2 class="title">${title}</h2><p>${description}</p>`
+  );
+
+  document.body.append(block);
+
+  setTimeout(() => block.remove(), 2000);
+};
+
+pushNotification(
+  10,
+  10,
+  'Attention!',
+  'Very interesting thing here',
+  'success'
+);
+
+pushNotification(
+  120,
+  10,
+  'Attention!!',
+  'Very bad thing here',
+  'warning'
+);
+
+pushNotification(
+  230,
+  10,
+  'Attention!!!!',
+  'Houston, we have a problems here!!',
+  'error'
+);
