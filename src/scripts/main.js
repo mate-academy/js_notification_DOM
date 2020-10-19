b168a6bebb1477d8fc5084095aa23656515cdd3f
Lancaelot <Lancaelot@gmail.com>
2020-10-19 12:03:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const notificationBlock = document.createElement('div');
+
+  notificationBlock.className = `notification ${type}`;
+
+  notificationBlock.innerHTML = (`
+  <h2>
+    ${title}
+  </h2>
+  <p>
+    ${description}
+  </p>
+  `);
+  notificationBlock.style.cssText = `top: ${top}; right: ${right};`;
+  document.body.append(notificationBlock);
+  setTimeout(() => notificationBlock.setAttribute('hidden', true), 2000);
+};
+
+pushNotification(
+  15,
+  15,
+  'Title',
+  'This is example of result.'
+  + 'Notification should contain title and description',
+  'warning'
+);
