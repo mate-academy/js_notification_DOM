2fa7c55bb2c682cbec52d9a91084f5d332da2890
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-20 14:26:44 +0300

    Notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (top, right, title, description, type) => {
+  const banner = document.createElement('div');
+  const bannerTitle = document.createElement('h2');
+  const bannerText = document.createElement('p');
+
+  bannerTitle.setAttribute('class', 'title');
+  bannerTitle.innerText = title;
+  bannerText.innerText = description;
+
+  banner.setAttribute('class', `notification ${type}`);
+  banner.append(bannerTitle);
+  banner.append(bannerText);
+
+  banner.style.top = top + 'px';
+  banner.style.right = right + 'px';
+
+  document.body.append(banner);
+
+  setTimeout(() => banner.remove(), 2000);
+};
+
+pushNotification(10, 20, 'Some banner',
+  'This is example of banner description', 'error');
