34a40f3d528d63549a20070f7e2916013feb3355
Ihor Oliinyk <i.oliinyk@mate.academy>
2020-11-03 13:32:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,32 @@
 'use strict';
-    const pushNotification = (top, right, title, description, type) => {
-    const main = document.querySelector('body');
-    const message = document.createElement('div');
-    const titleMessage = document.createElement('h2');
-    const descriptionMessage = document.createElement('p');
 
-    message.classList.add('notification', type);
-    message.style.cssText = `top: ${top}px; right: ${right}px`;
-    titleMessage.classList.add('title');
-    titleMessage.innerText = title;
-    descriptionMessage.innerText = description;
-      message.append(titleMessage, descriptionMessage);
-      main.append(message);
-    
-      window.setTimeout(function() {
-        message.remove(titleMessage, descriptionMessage);
-      }, 2000);
-    };
-    
-    pushNotification(10, 10, 'Title of Success message',
-      'This example of result.\n '
-    + 'Notification should contain title and description.', 'success');
-    
-    pushNotification(150, 10, 'Title of Error message',
-      'This example of result.\n '
-    + 'Notification should contain title and description.', 'error');
-    
-    pushNotification(290, 10, 'Title of Warning message',
-      'This example of result.\n '
-    + 'Notification should contain title and description.', 'warning');
-    
+const pushNotification = (top, right, title, description, type) => {
+  const main = document.querySelector('body');
+  const message = document.createElement('div');
+  const titleMessage = document.createElement('h2');
+  const descriptionMessage = document.createElement('p');
+
+  message.classList.add('notification', type);
+  message.style.cssText = `top: ${top}px; right: ${right}px`;
+  titleMessage.classList.add('title');
+  titleMessage.innerText = title;
+  descriptionMessage.innerText = description;
+  message.append(titleMessage, descriptionMessage);
+  main.append(message);
+
+  window.setTimeout(function() {
+    message.remove(titleMessage, descriptionMessage);
+  }, 2000);
+};
+
+pushNotification(10, 10, 'Title of Success message',
+  'This example of result.\n '
++ 'Notification should contain title and description.', 'success');
+
+pushNotification(150, 10, 'Title of Error message',
+  'This example of result.\n '
++ 'Notification should contain title and description.', 'error');
+
+pushNotification(290, 10, 'Title of Warning message',
+  'This example of result.\n '
++ 'Notification should contain title and description.', 'warning');
