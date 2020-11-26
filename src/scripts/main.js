3cb2be1553f79855225550967db1877933f3a887
Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>
2020-11-26 14:35:04 +0200

    js_notification_DOM: Cypress tests added (#113)
    * tests added, readme changed
    
    * typos fixed
    
    * small fix
    
    * tests refactored
    
    * tests refactored
    
    * tests and readme improved, picture renamed
    
    * readme and picture edited, main.js uncommented
    
    * inputs renamed to Pos
    
    * changed to posTop
    
    Co-authored-by: y.kostenko <y.kostenko@mate.academy>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-// const pushNotification = (top, right, title, description, type) => {
-//   // write code here
-// };
+const pushNotification = (posTop, posRight, title, description, type) => {
+  // write code here
+};
+
+pushNotification(10, 10, 'Title of Success message',
+  'Message example.\n '
+  + 'Notification should contain title and description.', 'success');
+
+pushNotification(150, 10, 'Title of Error message',
+  'Message example.\n '
+  + 'Notification should contain title and description.', 'error');
+
+pushNotification(290, 10, 'Title of Warning message',
+  'Message example.\n '
+  + 'Notification should contain title and description.', 'warning');
