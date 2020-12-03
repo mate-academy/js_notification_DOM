d4cb9d36f20377402a052cdc5966995774bb51a7
losanov <59749437+losanov@users.noreply.github.com>
2020-12-03 14:24:48 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,13 +15,13 @@ const pushNotification = (top, right, title, description, type) => {
 };
 
 pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n ' +
-  'Notification should contain title and description.', 'success');
+  'Message example.\n '
++ 'Notification should contain title and description.', 'success');
 
 pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n ' +
-  'Notification should contain title and description.', 'error');
+  'Message example.\n '
++ 'Notification should contain title and description.', 'error');
 
 pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n ' +
-  'Notification should contain title and description.', 'warning');
+  'Message example.\n '
++ 'Notification should contain title and description.', 'warning');
