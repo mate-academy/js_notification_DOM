d29a57a55e5967ef598563a4a567ec2f5d71472f
Your Name <mogolgogol.90@gmail.com>
2021-01-02 18:24:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+
+  const body = document.querySelector('body');
+  const message = document.createElement('div');
+
+  message.classList.add('notification', type);
+  body.append(message);
+
+  message.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>`;
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  setTimeout(() => {
+    message.style.display = 'none';
+  }, 2000);
+
 };
 
 pushNotification(10, 10, 'Title of Success message',
