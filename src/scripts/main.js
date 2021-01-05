4bf605ccde45cedf5186901ad08a2c98b547a835
5Mountains <V.Piatyhor@gmail.com>
2021-01-06 00:14:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,20 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+
+  block.classList.add('notification', type);
+
+  block.style.cssText = `top: ${posTop}px; right: ${posRight}px`;
+
+  document.body.insertAdjacentElement('afterbegin', block);
+
+  block.insertAdjacentHTML('afterbegin',
+    `<h2 class="title">${title}</h2>
+    <p>${description}</p>`
+  );
+
+  setTimeout(() => block.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
