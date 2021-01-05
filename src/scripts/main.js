7ae64c397e3bb831eee0a4aebd59551321af8f6d
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-05 23:25:08 +0200

    easyWay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,14 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   const body = document.querySelector('body');
+  body.insertAdjacentHTML('beforeend', `
+  <div class = "notification ${type}"
+  style = "top: ${posTop}px; right: ${posRight}px">
+  <h2 class = "title">${title}</h2>
+  <p>${description}</p>
+  </div>
+  `)
 
-  const el = document.createElement('div');
-
-  el.classList.add('notification', type);
-  el.style.top = `${posTop}px`;
-  el.style.right = `${posRight}px`;
-
-  const header = document.createElement('h2');
-
-  header.classList.add('title');
-  header.innerText = title;
-
-  const text = document.createElement('p');
-
-  text.innerText = description;
-
-  el.append(header);
-  el.append(text);
-
-  body.append(el);
   const notifications = document.querySelectorAll('.notification');
 
   setTimeout(() => notifications.forEach(message => message.remove()), 2000);
