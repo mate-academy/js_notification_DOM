e1621271c2e0b541a4cc990e99b54f5cc226c9c1
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-08 23:26:48 +0200

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,29 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const bodyElemnt = document.querySelector('body');
+  const notificationElement = document.createElement('div');
+  const notificationTitle = document.createElement('h2');
+  const notificationDescription = document.createElement('p');
+
+  const standartClassName = 'notification';
+
+  notificationElement.classList.add(standartClassName, type);
+
+  notificationElement.style.top = `${posTop}px`;
+  notificationElement.style.right = `${posRight}px`;
+  notificationTitle.style.fontStyle = 'normal';
+
+  notificationElement.append(notificationTitle, notificationDescription);
+  notificationTitle.className = 'title';
+
+  notificationTitle.innerText = title;
+
+  notificationDescription.innerText = description;
+
+  bodyElemnt.append(notificationElement);
+
+  setTimeout(() => notificationElement.remove(), 1999);
 };
 
 pushNotification(10, 10, 'Title of Success message',
