84dd8c551534a6f6d46d99b9ccf2c9c68f74c0ff
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-09 14:35:44 +0200

    appear disappear messages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,29 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  // console.log(posTop, posRight, title, description, type);
+  const place = document.querySelector('body');
+  const element = document.createElement('div');
+  const titleText = document.createElement('h2');
+  const messageText = document.createElement('p');
+
+  element.classList.add('notification');
+  element.classList.add(type);
+  element.style.top = posTop + 'px';
+  element.style.right = posRight + 'px';
+
+  titleText.classList.add('title');
+  titleText.textContent = `${title}`;
+
+  messageText.textContent = `${description}`;
+
+  place.append(element);
+  element.append(titleText);
+  element.append(messageText);
+
+  setTimeout(() => {
+    element.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
