0085cde63dc1d22b5bc5632eeb0048964bd1a169
Anna <garsinaanna32778@gmail.com>
2021-03-10 09:57:45 +0200

    created notificationa

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,34 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const message = document.createElement('div');
+  const paragraph = document.createElement('p');
+  const header = document.createElement('h2');
+
+  message.className = `notification ${type}`;
+  header.setAttribute('class', 'title');
+  header.innerHTML = `${title}`;
+  paragraph.innerHTML = `${description}`;
+
+  message.append(header);
+  message.append(paragraph);
+
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  document.body.append(message);
+
+  setTimeout(() => message.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+  + 'Notification should contain header and paragraph.', 'success');
 
 pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+  + 'Notification should contain header and paragraph.', 'error');
 
 pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+  + 'Notification should contain header and paragraph.', 'warning');
