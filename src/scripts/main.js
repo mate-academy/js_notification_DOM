73e6dda29166024cd1bf17453ad8d47c713f1cfc
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-10 10:52:11 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,38 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  const message = document.createElement('div');
+  const notification = document.createElement('div');
   const heading = document.createElement('h2');
   const text = document.createElement('p');
 
-  message.classList.add('notification', type);
+  notification.classList.add('notification', type);
   heading.className = 'title';
 
   heading.innerText = title;
   text.innerText = description;
 
-  message.style.top = posTop + 'px';
-  message.style.right = posRight + 'px';
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
 
   heading.style.fontSize = '17px';
   text.style.fontSize = '15px';
 
-  document.body.append(message);
-  message.append(heading, text);
+  document.body.append(notification);
+  notification.append(heading, text);
 
   setTimeout(() => {
-    message.remove();
+    notification.remove();
   }, 2000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
+pushNotification(10, 10, 'Title of Success notification',
+  'notification example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
+pushNotification(150, 10, 'Title of Error notification',
+  'notification example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
+pushNotification(290, 10, 'Title of Warning notification',
+  'notification example.\n '
   + 'Notification should contain title and description.', 'warning');
