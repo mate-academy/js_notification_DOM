4ed228d31c8548d6f7a8f7abd936e5e09cda1fd6
Artur Shpontak <ashpontak97@gmail.com>
2021-03-10 14:22:33 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,51 @@
 'use strict';
 
+class NotificationMessage {
+  constructor(type, positionTop, positionRight) {
+    const newNotification = document.createElement('div');
+
+    newNotification.classList.add('notification', type);
+    newNotification.style.top = `${positionTop}px`;
+    newNotification.style.right = `${positionRight}px`;
+
+    this.content = newNotification;
+
+    return this;
+  }
+
+  addElement(tagName, classOfElement, textValue) {
+    const createdElement = document.createElement(tagName);
+
+    createdElement.className = classOfElement;
+    createdElement.innerText = textValue;
+
+    if (classOfElement === 'title') {
+      createdElement.style.fontSize = '16px';
+    }
+
+    this.content.append(createdElement);
+  }
+}
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = new NotificationMessage(type, posTop, posRight);
+
+  notification.addElement('h2', 'title', title);
+  notification.addElement('p', 'description', description);
+
+  document.body.append(notification.content);
+
+  setTimeout(() => notification.content.remove(), 2000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
+pushNotification(10, 10, 'Title of Success notification',
+  'notification example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
+pushNotification(150, 10, 'Title of Error notification',
+  'notification example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
+pushNotification(290, 10, 'Title of Warning notification',
+  'notification example.\n '
   + 'Notification should contain title and description.', 'warning');
