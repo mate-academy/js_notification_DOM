e6ecb5b033ee46bb99ba03805688d9172dfb044c
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-25 15:29:25 +0200

    Added notifications

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const pageTitle = document.querySelector('.logo');
+const body = document.body;
+const notificationsWrapper = document.createElement('div');
+
+body.insertBefore(notificationsWrapper, pageTitle);
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  notificationsWrapper.insertAdjacentHTML('beforeend', `
+  <div class = 'notification ${type}'>
+    <h2 class = 'title'>${title}</h2>
+    <p>${description}</p>
+  </div>`);
+
+  const notification = notificationsWrapper.querySelector(`.${type}`);
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
 };
 
 pushNotification(10, 10, 'Title of Success message',
@@ -15,3 +30,5 @@ pushNotification(150, 10, 'Title of Error message',
 pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
+
+setTimeout(() => notificationsWrapper.remove(), 2000);
