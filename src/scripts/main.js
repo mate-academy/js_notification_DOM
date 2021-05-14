d3534b46fbf0940c591d937a37c1c46c40907275
romandubovskyi <rdubowskij@gmail.com>
2021-05-14 09:46:39 +0300

    added notifications

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('span');
+
+  notification.classList.add('notification');
+  notification.classList.add(type);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  notification.innerHTML = `<h2 class="title">
+      ${title}
+  </h2>
+  <p>
+  ${description}
+  </p>
+  `;
+  document.body.appendChild(notification);
+  setTimeout(() => document.body.removeChild(notification), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
