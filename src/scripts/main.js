866e5ed8840e5e004f55fc5ecd1e63d41246cceb
David Dohru <tronsmartmy@gmail.com>
2021-07-11 20:26:28 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+  const titleText = document.createElement('h2');
+  const text = document.createElement('p');
+
+  block.classList.add(`notification`, type);
+  titleText.innerText = title;
+  titleText.style.fontSize = '20px';
+  text.innerText = description;
+  block.style.cssText = `top:${posTop}px;right:${posRight}px`;
+  block.append(titleText, text);
+  document.body.append(block);
+  setTimeout(() => (block.remove()), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
