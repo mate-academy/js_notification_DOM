3a260376924ad612d04082c8e3bc35ce35fdeb4a
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-03 17:11:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,29 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const notifTitle = document.createElement('h2');
+  const notifDescrb = document.createElement('p');
+
+  notification.className = `notification ${type}`;
+  notification.style.width = '320px';
+  notification.style.height = 'min-content';
+  notification.style.right = `${posRight}px`;
+  notification.style.top = `${posTop}px`;
+  notification.style.padding = '10px';
+
+  notifTitle.className = 'title';
+  notifTitle.textContent = `${title}`;
+  notifDescrb.textContent = `${description}`;
+
+  document.body.append(notification);
+  notification.append(notifTitle);
+  notification.append(notifDescrb);
+
+  setTimeout(function() {
+    notification.setAttribute('hidden', 'hidden');
+  },
+  2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
