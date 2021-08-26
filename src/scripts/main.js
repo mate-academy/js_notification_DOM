8c7e4608a7b132e24e851e965d470e6f95249d47
Roman Minchenko <minchenko.poma@gmail.com>
2021-08-26 16:55:44 +0300

    nptification-DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,35 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+
+  document.body.append(block);
+  block.className = `notification ${type}`;
+  block.style.top = `${posTop}px`;
+  block.style.right = `${posRight}px`;
+  block.style.width = 'auto';
+  block.style.height = '130px';
+
+  const titleBlock = document.createElement('h2');
+
+  block.append(titleBlock);
+  titleBlock.textContent = `${title}`;
+  titleBlock.classList.add('title');
+
+  const descriptionBlock = document.createElement('p');
+
+  block.append(descriptionBlock);
+  descriptionBlock.textContent = `${description}`;
+
+  const deleteElement = (i) => {
+    const displayNone = document.body.children[i].style.display = 'none';
+
+    return displayNone;
+  };
+
+  for (let i = 2; i < 5; i++) {
+    setTimeout(deleteElement, 1000 * `${i}`, `${i}`);
+  }
 };
 
 pushNotification(10, 10, 'Title of Success message',
