5b31d6f8b889b6cde94df7341c3ad581281e60a5
Anton Forzun <anton_forzun@ukr.net>
2021-09-24 13:34:27 +0300

    notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,50 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+
+  const div = document.createElement('div');
+
+  if (type === 'success') {
+    div.className = 'success';
+    div.style.backgroundColor = '#c0ddb6';
+
+    setTimeout(() => {
+      div.style.display = 'none';
+    }, 2000);
+  }
+
+  if (type === 'error') {
+    div.className = 'error';
+    div.style.backgroundColor = '#ecb5b1';
+
+    setTimeout(() => {
+      div.style.display = 'none';
+    }, 4000);
+  }
+
+  if (type === 'warning') {
+    div.className = 'warning';
+    div.style.backgroundColor = '#f1e5bf';
+
+    setTimeout(() => {
+      div.style.display = 'none';
+    }, 6000);
+  }
+
+  div.style.borderRadius = '10px';
+  div.style.padding = '10px';
+  div.style.display = 'block';
+  div.style.position = 'fixed';
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  div.innerHTML = `
+    <h2>${title}</h2>
+    <p>${description}</p>
+  `;
+
+  body.append(div);
 };
 
 pushNotification(10, 10, 'Title of Success message',
