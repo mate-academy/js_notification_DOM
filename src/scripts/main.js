27f0f0f470a661b39c56970da60d3544133f3cd6
bohdan_kochetov <bodja1208@gmail.com>
2021-09-30 19:03:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,51 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  switch (type) {
+    case 'success' :
+      div.classList = 'notification success';
+      break;
+    case 'warning' :
+      div.classList = 'notification warning';
+      break;
+    case 'error' :
+      div.classList = 'notification error';
+      break;
+  }
+  body.append(div);
+
+  const h2 = document.createElement('h2');
+
+  h2.className = 'title';
+  h2.innerText = title;
+  div.prepend(h2);
+
+  const p = document.createElement('p');
+
+  p.innerText = description;
+  div.append(p);
+
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
 };
+
+setTimeout(() => {
+  const div = document.querySelector('div');
+
+  div.remove();
+}, 2000);
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(180, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(320, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
