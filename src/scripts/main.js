714d239f8904cd5fac4f3c62292eae2ca0ecaef3
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2022-01-10 10:47:42 +0200

    new solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const pushNotification = (posTop, posRight, title, description, type) => {
   const newDiv = `
   <div class="notification ${type}"
-    style="top :${posTop}px; right: ${posRight}px;>
+    style="top :${posTop}px; right: ${posRight}px;">
   <h2 class="title">${title}</h2>
   <p>${description}</p>
   </div>
@@ -20,7 +20,7 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(165, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
