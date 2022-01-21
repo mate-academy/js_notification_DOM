a0e2acf86c96e6ba54a4668ff48eed5a81cea51b
Victor Hrygorash <viktor12041991@i.ua>
2022-01-21 14:22:39 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,30 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const message = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  message.classList.add('notification', `${type}`);
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+  h2.classList.add('title');
+  h2.textContent = title;
+  p.textContent = description;
+  message.append(h2, p);
+  body.append(message);
+
+  setTimeout(() => {
+    message.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(165, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
