0109bb95e8b8becf53400da3664c46ea226d692a
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-06 11:05:55 +0200

    add function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,28 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notification = document.createElement('div');
+
+  notification.className = 'notification';
+  notification.classList.add(type);
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+  notification.style.boxSizing = `content-box`;
+
+  const h2 = document.createElement('h2');
+
+  h2.textContent = title;
+  h2.className = 'title';
+  notification.append(h2);
+
+  const p = document.createElement('p');
+
+  p.innerText = description;
+  notification.append(p);
+
+  document.body.append(notification);
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
