4bea73010909cdf462736a59858c0ae4820c85ca
Sasha Pavlenko <380971520240@gmail.com>
2022-02-07 23:20:38 +0200

    My solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,30 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const page = document.querySelector('body');
+
+  page.insertAdjacentHTML('beforeend', `
+    <div class="notification ${type}"
+      style="top:${posTop}px; right:${posRight}px;">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `);
+
+  setTimeout(() => {
+    const item = document.querySelectorAll('.notification');
+
+    for (const key of item) {
+      key.remove();
+    };
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(165, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
