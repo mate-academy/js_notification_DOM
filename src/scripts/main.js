7eb047c574ca14c74358d2615f818e5145a2ee92
Maksym Yaremenko <restartermord123@ukr.net>
2022-04-14 14:51:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,34 @@
 'use strict';
 
+const container = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const errorBox = document.createElement('div');
+
+  errorBox.classList.add('notification');
+  errorBox.classList.add(`${type}`);
+
+  errorBox.style.top = `${posTop}px`;
+  errorBox.style.right = `${posRight}px`;
+  // without changing the width everything breaks
+  errorBox.style.width = `315px`;
+
+  const h2 = document.createElement('h2');
+
+  h2.classList.add('title');
+  h2.innerText = title;
+  errorBox.prepend(h2);
+
+  const p = document.createElement('p');
+
+  p.innerText = description;
+  errorBox.append(p);
+
+  container.append(errorBox);
+
+  setTimeout(() => {
+    errorBox.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
