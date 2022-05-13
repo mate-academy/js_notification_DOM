8e86f49ec9af71b4ab40de392fb5b1a940675ff3
Roman Nikitenko <Lomkaaa@ukr.net>
2022-05-13 10:41:22 +0300

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const h2 = document.createElement('h2');
+  const block = document.createElement('div');
+  const paragraph = document.createElement('p');
+
+  block.className = `notification ${type}`;
+  h2.textContent = title;
+  h2.className = 'title';
+  paragraph.textContent = description;
+  block.style.top = `${posTop}px`;
+  block.style.right = `${posRight}px`;
+  block.append(h2, paragraph);
+
+  setTimeout(() => {
+    body.append(block);
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
