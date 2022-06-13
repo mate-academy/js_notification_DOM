ea31911d7e71f4f5331d6d14d64054a628f2ed12
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-06-13 15:45:45 +0300

    Fixed h2 tag and used DRY

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,20 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  const element = document.querySelector('body')
-  if (type === 'success') {
-    element.insertAdjacentHTML('beforeend',`
-    <div class="notification success">
-    <h2 class="title">${title}<h2/>
-    <p>${description}</p>
-    </div>
-    `);
-  
-    const suc = document.querySelector('.success')
-    suc.style.top = posTop.toString() + 'px'
-    suc.style.right = posRight.toString() + 'px'
+  const element = document.querySelector('body');
 
-  } 
-  else if (type === 'error') {
-    element.insertAdjacentHTML('beforeend',`
-    <div class="notification error">
-    <h2 class="title">${title}<h2/>
+  element.insertAdjacentHTML('beforeend', `
+  <div class="notification ${type}" 
+  style="top: ${posTop}px; right: ${posRight}px">
+    <h2 class="title">${title}</h2>
     <p>${description}</p>
-    </div>
-    `);
-  
-    const suc = document.querySelector('.error')
-    suc.style.top = posTop.toString() + 'px'
-    suc.style.right = posRight.toString() + 'px'
+  </div>
+  `);
 
-  } else{
-    element.insertAdjacentHTML('beforeend',`
-    <div class="notification warning">
-    <h2 class="title">${title}<h2/>
-    <p>${description}</p>
-    </div>
-    `);
-  
-    const suc = document.querySelector('.warning')
-    suc.style.top = posTop.toString() + 'px'
-    suc.style.right = posRight.toString() + 'px'
-  }
+  setTimeout(() => {
+    const elementForRemove = document.querySelector('.notification');
 
-  setTimeout(()=>{
-    const elementForRemove = document.querySelector('.notification')
-    elementForRemove.remove()
+    elementForRemove.remove();
   }, 2000
   );
 };
@@ -51,10 +23,10 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(250, 10, 'Title of Error message',
+pushNotification(165, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(460, 10, 'Title of Warning message',
+pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
