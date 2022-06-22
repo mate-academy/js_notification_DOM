e04cb3f37532982d7066127dc312a98e8f687ff1
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-06-22 19:34:59 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,30 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const element = document.createElement('div');
+  const itemTitle = document.createElement('h2');
+  const itemText = document.createElement('p');
+
+  setTimeout(() => {
+    itemTitle.textContent = title;
+    itemTitle.className = 'title';
+
+    itemText.textContent = description;
+
+    element.classList.add('notification', type);
+
+    element.style.top = `${parseFloat(posTop)}px`;
+    element.style.right = `${parseFloat(posRight)}px`;
+
+    element.append(itemTitle);
+    element.append(itemText);
+    body.append(element);
+
+    element.style.boxSizing = `content-box`;
+
+    itemText.style.whiteSpace = 'pre-line';
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
