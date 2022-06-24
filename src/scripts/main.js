bd746d5e75db7fa9932df7ef998d6f3882529880
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-24 12:58:50 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,31 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification');
+  notification.classList.add(type);
+
+  const h2 = document.createElement('h2');
+
+  h2.className = 'title';
+  h2.append(title);
+
+  const p = document.createElement('p');
+
+  p.append(description);
+
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  body.append(notification);
+  notification.append(h2);
+  notification.append(p);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
