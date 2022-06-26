40d8733556c5cae2f229523c91872a3e6d9f1aa1
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-26 12:15:49 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,25 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  const body = document.querySelector('body');
+
+  h2.textContent = title;
+  h2.className = 'title';
+  p.textContent = description;
+
+  div.className = 'notification';
+  div.classList.add(type);
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+  div.style.boxSizing = 'content-box';
+
+  div.append(h2);
+  div.append(p);
+  body.append(div);
 };
 
 pushNotification(10, 10, 'Title of Success message',
