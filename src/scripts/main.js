e1e0de6c6b82eaa109b58f6bda065ff1569b3a98
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-07-09 23:12:04 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const bodyElem = document.querySelector('body');
+  const divElem = document.createElement('div');
+  const h2Elem = document.createElement('h2');
+  const pElem = document.createElement('p');
+
+  divElem.style.top = `${posTop}px`;
+  divElem.style.right = `${posRight}px`;
+  divElem.className = `notification ${type}`;
+  h2Elem.className = 'title';
+  h2Elem.innerText = title;
+  pElem.innerText = description;
+  bodyElem.append(divElem);
+  divElem.append(h2Elem);
+  divElem.append(pElem);
+
+  setTimeout(() => {
+    divElem.hidden = true;
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
