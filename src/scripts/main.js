bebacba3f1f2f8197fada5261d773590ee266d4b
Max Shylenko <maxshylenko@gmail.com>
2022-07-11 19:27:05 +0200

    notification DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,30 @@
 'use strict';
 
+const page = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div');
+
+  message.className = `notification ${type}`;
+
+  message.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  page.append(message);
+
+  setTimeout(() => message.remove(), 2000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+pushNotification(10, 10, 'Success!!!',
+  'Enter valid data.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+pushNotification(150, 10, 'Error!!!',
+  'Enter invalid data.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+pushNotification(290, 10, 'Warning!!!',
+  'Mistake, please, correct data.', 'warning');
