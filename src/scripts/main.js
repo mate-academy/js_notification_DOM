d27808fcf671330cf0692033c25015c9c72e3623
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-07-12 18:47:31 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,21 @@
 const pushNotification = (posTop, posRight, title, description, type) => {
   const bodyElem = document.querySelector('body');
   const divElem = document.createElement('div');
-  const h2Elem = document.createElement('h2');
-  const pElem = document.createElement('p');
 
   divElem.style.top = `${posTop}px`;
   divElem.style.right = `${posRight}px`;
   divElem.className = `notification ${type}`;
-  h2Elem.className = 'title';
-  h2Elem.innerText = title;
-  pElem.innerText = description;
+
+  divElem.innerHTML = `
+    <h2 class="title">
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  `;
+
   bodyElem.append(divElem);
-  divElem.append(h2Elem);
-  divElem.append(pElem);
 
   setTimeout(() => {
     divElem.hidden = true;
