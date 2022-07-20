f73b4fde7dd70876d14963972185978427c359ef
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-20 14:54:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,33 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const infoBlock = document.createElement('div');
+  const titleInfo = document.createElement('h2');
+  const descriptionInfo = document.createElement('p');
+
+  infoBlock.classList.add('notification', type);
+  titleInfo.classList.add('title');
+
+  titleInfo.textContent = title;
+  descriptionInfo.textContent = description;
+
+  infoBlock.append(titleInfo, descriptionInfo);
+
+  infoBlock.style.top = posTop + 'px';
+  infoBlock.style.right = posRight + 'px';
+
+  document.body.append(infoBlock);
+
+  setTimeout(() => {
+    infoBlock.remove();
+  }, 3000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(160, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
