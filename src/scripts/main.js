b85619126b8a09d5aea94cc25969e8271ee0bb8f
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-20 16:57:57 +0300

    Change to 150

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,7 +27,7 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(160, 10, 'Title of Error message',
+pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
