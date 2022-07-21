153c29008246ef75a7875e77032d9487405ccaf9
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-21 21:28:31 +0300

    Notifications has been implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,32 @@
 'use strict';
 
+const root = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const messageBlock = document.createElement('div');
+
+  messageBlock.classList.add('notification', type);
+
+  messageBlock.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+
+  messageBlock.style.cssText = `
+    top: ${[...messageBlock.classList].includes('error')
+    ? posTop + 10
+    : posTop}px;
+    right: ${posRight}px;
+  `;
+
+  root.append(messageBlock);
+
+  setTimeout(() => messageBlock.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
++ 'Notification should contain title and description.', 'success');
 
 pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
