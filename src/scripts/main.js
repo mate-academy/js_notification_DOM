53d15c54a2269d40df95d939777fc7d02a809b99
Oleg Vetrov <oleg94038@gmail.com>
2022-07-23 18:53:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,38 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  document.body.insertAdjacentHTML('beforeend', `
+  <div class="notification ${type}"
+       style="position: absolute;
+       top: ${posTop}px;
+       right: ${posRight}px;"
+  >
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </div>
+  `);
+
+  const notification = document.querySelector(`.${type}`);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+setTimeout(() => {
+  pushNotification(10, 10, 'Title of Success message',
+    'Message example.\n '
+    + 'Notification should contain title and description.', 'success');
+}, 1000);
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+setTimeout(() => {
+  pushNotification(164, 10, 'Title of Error message',
+    'Message example.\n '
+    + 'Notification should contain title and description.', 'error');
+}, 2000);
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+setTimeout(() => {
+  pushNotification(290, 10, 'Title of Warning message',
+    'Message example.\n '
+    + 'Notification should contain title and description.', 'warning');
+}, 3000);
