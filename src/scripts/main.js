aa8416441190730a0f736e459ce6ee30bedb77c2
Andrii Kuzmenko <andriikuzmenko35@gmail.com>
2022-08-09 13:45:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  document.body.children[0].insertAdjacentHTML('afterend', `
+  <div class="notification ${type}">
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  </div>
+  `);
+
+  const notificationElement = document.querySelector('.notification');
+
+  notificationElement.style.top = `${posTop}px`;
+  notificationElement.style.right = `${posRight}px`;
+
+  notificationElement.style.boxSizing = 'content-box';
+
+  const remover = () => notificationElement.remove();
+
+  setTimeout(remover, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
