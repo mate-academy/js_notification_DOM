e9ecba983c89171a181386ad0680eae645609604
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-08-18 23:35:09 +0300

    Notification DOM task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const messageBlock = document.createElement('div');
+
+  messageBlock.style.top = `${posTop}px`;
+  messageBlock.style.right = `${posRight}px`;
+  // messageBlock.style.boxSizing = 'content-box';
+  messageBlock.classList.add('notification', type);
+
+  const titleMessage = document.createElement('h2');
+
+  titleMessage.classList.add('title');
+  titleMessage.innerText = title;
+
+  const messageText = document.createElement('p');
+
+  messageText.innerText = description;
+
+  messageBlock.append(titleMessage, messageText);
+  document.body.lastChild.before(messageBlock);
+
+  setTimeout(() => {
+    messageBlock.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
