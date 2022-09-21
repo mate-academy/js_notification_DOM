07c49182f8b45068517a1191cf4029ce4a3d6c15
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-09-22 00:57:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,49 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const page = document.querySelector('body');
+
+  const notification = document.createElement('div');
+
+  notification.style.top = posTop + 'px';
+
+  notification.style.right = posRight + 'px';
+
+  notification.classList.add('notification');
+
+  switch (type) {
+    case 'success':
+      notification.classList.add('success');
+      break;
+
+    case 'error':
+      notification.classList.add('error');
+      break;
+
+    case 'warning':
+      notification.classList.add('warning');
+      break;
+  }
+
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.innerText = title;
+
+  notificationTitle.classList.add('title');
+
+  notificationTitle.style.fontSize = '18px';
+
+  notification.append(notificationTitle);
+
+  const notificationDescription = document.createElement('p');
+
+  notificationDescription.innerText = description;
+
+  notification.append(notificationDescription);
+
+  page.append(notification);
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
