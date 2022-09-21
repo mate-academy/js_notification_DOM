4939a175bb88a05fc25c074c2c7d158047b2210b
grafit-off <graf.off.it@gmail.com>
2022-09-21 11:53:08 +0300

    feat: notification func completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,29 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+
+  notification.innerHTML = `
+    <h2 class="title">
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  `;
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+  notification.style.transition = `opacity .3s, visibility .3s`;
+
+  document.body.insertAdjacentElement('beforeend', notification);
+
+  setTimeout(() => {
+    notification.style.opacity = '0';
+    notification.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
