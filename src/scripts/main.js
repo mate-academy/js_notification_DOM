7f30f5d1f0014596d935c01e80d7829e6cbf9555
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-27 17:28:31 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.body;
+
+  const message = `
+    <div 
+      class="notification ${type}" 
+      style="top: ${posTop}px; right: ${posRight}px; box-sizing: content-box">
+        <h2 class="title">${title}</h2>
+        <p>${description.slice(0, 16)}<br>${description.slice(17, -1)}</p>
+    </div>
+  `;
+
+  body.insertAdjacentHTML('afterbegin', message);
 };
 
 pushNotification(10, 10, 'Title of Success message',
