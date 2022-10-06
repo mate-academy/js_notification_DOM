ac7640f7fe70f7e7eeeb9d4311f141867b7f03a1
Zykhor Andrii <zuxoran@gmail.com>
2022-10-06 19:14:27 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,31 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+
+  const h2 = document.createElement('h2');
+  const text = document.createElement('p');
+  const wrap = document.createElement('div');
+
+  h2.classList.add('title');
+  h2.textContent = title;
+
+  text.textContent = description;
+
+  wrap.classList.add('notification', type);
+
+  wrap.append(h2, text);
+
+  wrap.style.top = `${posTop}px`;
+
+  wrap.style.right = `${posRight}px`;
+
+  wrap.style.boxSizing = 'content-box';
+
+  document.body.append(wrap);
+
+  setTimeout(() => {
+    wrap.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
