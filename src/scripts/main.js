bf992381b4ab9ebae3f5199b50d8fea857d35fdf
illia_kots <illyahennadievich@gmail.com>
2022-10-12 17:26:46 +0300

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const page = document.querySelector('body');
+  const newElement = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageParagraph = document.createElement('p');
+
+  newElement.className = `notification ${type}`;
+  newElement.style.position = 'absolute';
+  newElement.style.boxSizing = 'content-box';
+  newElement.style.top = `${posTop}px`;
+  newElement.style.right = `${posRight}px`;
+
+  messageTitle.className = 'title';
+  messageTitle.textContent = title;
+
+  messageParagraph.textContent = description;
+
+  newElement.append(messageTitle, messageParagraph);
+
+  page.append(newElement);
+
+  setTimeout(() => newElement.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
