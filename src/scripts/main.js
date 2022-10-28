bc417600559f69eba740a163b05efca04046ab11
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-10-28 03:31:24 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,37 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.body;
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  h2.innerText = title;
+  p.innerText = description;
+  notification.append(div);
+  div.append(h2);
+  h2.append(p);
+
+  div.classList.add('notification', `${type}`);
+  h2.classList.add(`title`);
+  p.style = `font-weight: normal;`;
+
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  const remove = () => div.remove();
+
+  setTimeout(remove, 3000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(200, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(360, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
