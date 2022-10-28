d0cbb0d040cbdda2add25c35d21d8bbf6e701bf0
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-28 11:32:04 +0300

    cod after fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,10 +26,10 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(180, 10, 'Title of Error message',
+pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(320, 10, 'Title of Warning message',
+pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
