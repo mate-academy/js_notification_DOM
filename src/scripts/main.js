3dffefc195a5cd23c54fb7a48fbba5342cb319ec
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-19 17:02:57 +0200

    vol_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const correctDescription = description.replace(/\n/g, ' <br/>');
+
+  const bodyElem = document.querySelector('body');
+
+  bodyElem.insertAdjacentHTML('afterbegin', `
+    <div class="notification ${type}">
+      <h2 class="title">
+        ${title}
+      </h2>
+      <p>${correctDescription}</p>
+    </div>
+  `);
+
+  const divMargin = document.querySelector('div');
+
+  divMargin.style.marginTop = `${posTop}px`;
+  divMargin.style.marginLeft = `${posRight}px`;
+  divMargin.style.width = `max-content`;
 };
 
 pushNotification(10, 10, 'Title of Success message',
