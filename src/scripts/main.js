66d91fcc39253274a0a0ab76450eb97e764103fd
Olena Shevchenko <shevochkas@gmail.com>
2022-11-23 17:27:09 +0200

    notification was added with js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  
+  const body = document.querySelector('body');
+  const element = document.createElement('div');
+
+  element.className = 'notification';
+  element.classList.add(type);
+  element.style.top = posTop + 'px';
+  element.style.right = posRight + 'px';
+
+  element.insertAdjacentHTML('afterbegin', `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `);
+  body.append(element);
+
+  setTimeout(() => {
+    element.remove();
+  }, '2000');
 };
 
 pushNotification(10, 10, 'Title of Success message',
