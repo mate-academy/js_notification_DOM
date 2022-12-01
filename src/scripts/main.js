f85c37bac31c4e328a299a22fb8c557ab66b7e83
Elena Ver <hellena84@i.ua>
2022-12-01 15:11:31 +0100

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,36 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  const h2 = document.createElement('h2');
+
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  div.classList.add(`notification`, type);
+  h2.classList.add('title');
+
+  p.innerText = description;
+  h2.innerText = title;
+
+  div.append(h2);
+  div.append(p);
+  body.append(div);
+
+  setTimeout(() => div.remove(), 5000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(190, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(340, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
