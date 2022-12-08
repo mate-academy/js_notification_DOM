7f036b72d431b1f453d2f13c31fdd95d888792ca
dmitriy-kovalenko <kovalenkodmitrij@gmail.com>
2022-12-08 13:24:20 +0200

    work notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,31 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const elementDiv = document.createElement('div');
+  const elementH2 = document.createElement('h2');
+  const elementP = document.createElement('p');
+
+  elementDiv.className = `notification ${type}`;
+  elementH2.className = `title`;
+
+  elementH2.innerText = title;
+  elementP.innerText = description;
+
+  elementDiv.append(elementH2);
+  elementDiv.append(elementP);
+
+  elementDiv.style.width = 'max-content';
+  elementDiv.style.top = posTop + 'px';
+  elementDiv.style.right = posRight + 'px';
+
+  document.body.append(elementDiv);
+
+  const timerId = setInterval(() => {}, 0);
+
+  setTimeout(() => {
+    clearInterval(timerId);
+    elementDiv.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
