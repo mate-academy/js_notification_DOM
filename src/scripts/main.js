683573922411375e7d5a0e9a2f9d55b125c577cc
Oleksii Pishchuhin <aliliolek@gmail.com>
2022-12-24 03:30:41 +0100

    solution 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,26 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  let notification = document.createElement('div');
+  const body = document.querySelector('body');
+
+  notification.classList.add('notification', type);
+
+  notification.innerHTML += `
+    <h2 class="title">${title}</h2>
+    <p class="description">${description}</p>
+  `;
+
+  body.append(notification);
+
+  // to style notification
+  notification = document.querySelector(`.${type}`);
+
+  notification.style.cssText = `
+    box-sizing: content-box;;
+    right: ${posRight}px;
+    top: ${posTop}px;
+  `;
 };
 
 pushNotification(10, 10, 'Title of Success message',
