075d146448773dde1f8ad75e8a697aec1871b8cd
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-27 21:23:23 +0200

    styleFix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,8 +18,7 @@ const pushNotification = (posTop, posRight, title, description, type) => {
 
   setTimeout(() => {
     div.remove();
-  }
-  , 2000);
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
