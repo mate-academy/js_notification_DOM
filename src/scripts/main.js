20e3247ba68bb6ef44161cd7de4860c193cb8738
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-11 11:49:58 +0000

    notifications DOM task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,43 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const paragraph = document.createElement('p');
+
+  block.classList.add('notification');
+  h2.classList.add('title');
+  h2.style.fontSize = '16px';
+  block.style.top = `${posTop}px`;
+  block.style.right = `${posRight}px`;
+  block.append(h2);
+  block.append(paragraph);
+
+  h2.innerText = title;
+  paragraph.innerText = description;
+
+  switch (type) {
+    case 'success':
+      block.classList.add('success');
+      break;
+
+    case 'error':
+      block.classList.add('error');
+      break;
+
+    case 'warning':
+      block.classList.add('warning');
+      break;
+
+    default:
+      break;
+  }
+
+  document.body.append(block);
+
+  setTimeout(() => {
+    block.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
