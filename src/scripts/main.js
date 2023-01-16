19c8794a488d360775ad04b8b54ecf73d197f5a3
abobyr <artem.bobyr@dataart.com>
2023-01-16 18:48:42 +0200

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div')
+  message.classList.add('notification', type)
+  message.style.top = posTop+'px'
+  message.style.right = posRight+'px'
+
+  const titleElem = document.createElement('h2')
+  titleElem.classList.add('title')
+  titleElem.textContent = title
+
+  document.documentElement.append(message)
+  message.append(titleElem)
+  message.insertAdjacentHTML("beforeend", `<p>${description}</p>`)
+
+  setTimeout(() => {
+    message.style.display = 'none'
+  }, 2000)
 };
 
 pushNotification(10, 10, 'Title of Success message',
