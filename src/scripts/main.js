5d3f8be619a157d74cc6b846e29253ae6450a481
valntyn <romanenko.valentyn1@gmail.com>
2023-01-21 19:42:53 +0200

    solut

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,32 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div class="notification">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `);
+
+  const div = document.querySelector('div');
+
+  div.classList.add(type);
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  setTimeout(() => {
+    div.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(175, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(310, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
