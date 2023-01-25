64d8870a62faea7306e323661da9760061d95aa2
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-01-25 19:42:54 +0200

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const box = document.createElement('div');
+  const body = document.querySelector('body');
+  const h1 = document.querySelector('h1');
+
+  box.classList.add('notification', type);
+
+  box.insertAdjacentHTML('afterbegin',
+    `
+    <h2 class='title'>${title}</h2>
+    <p>${description}</p>
+    `
+  );
+
+  body.insertBefore(box, h1.nextSibling);
+
+  box.style.top = `${posTop}px`;
+  box.style.right = `${posRight}px`;
+  box.style.boxSizing = 'content-box';
+
+  setTimeout(() => box.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
