e4685a9b728158f70e67c24fae17decff86c6425
ihorivna007 <emmakorovnik@gmail.com>
2023-01-31 13:57:47 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,37 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const h1 = document.querySelector('h1');
+
+  h1.insertAdjacentHTML('afterbegin',
+    `<div class='notification ${type}'>
+      <h2 class='title'>${title}</h2>
+      <p>${description}</p>
+    </div>`
+  );
+
+  const element = document.querySelector(`.${type}`);
+
+  element.style.top = `${posTop}px`;
+  element.style.right = `${posRight}px`;
+
+  setTimeout(() => {
+    element.setAttribute('hidden', 'true');
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+setTimeout(() => {
+  pushNotification(150, 10, 'Title of Error message',
+    'Message example.\n '
+    + 'Notification should contain title and description.', 'error');
+}, 1000);
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+setTimeout(() => {
+  pushNotification(290, 10, 'Title of Warning message',
+    'Message example.\n '
+    + 'Notification should contain title and description.', 'warning');
+}, 2000);
