8eb94920cebda8d24e13ba9a2eb5061712f270ee
Mykola Huryn <mukola197022@gmail.com>
2023-02-09 18:14:33 +0100

    js_notification_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  body.append(div);
+  div.append(h2, p);
+
+  h2.textContent = title;
+  p.textContent = description;
+
+  div.classList.add('notification', type);
+  h2.classList.add('title');
+
+  div.style.position = 'absolute';
+  div.style.top = posTop + 'px';
+  div.style.right = posRight + 'px';
+
+  setTimeout(() => {
+    div.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
