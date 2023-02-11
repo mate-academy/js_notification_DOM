9e109106cfc97c193e4dbce4683308826166a08d
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-11 20:25:45 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,35 @@
 'use strict';
 
-const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
-};
+function pushNotification(posTop, posRight, title, description, type) {
+  const block = document.createElement('div');
+  const blockTitle = document.createElement('h2');
+  const blockText = document.createElement('p');
+
+  block.style.top = posTop + 'px';
+  block.style.right = posRight + 'px';
+
+  block.classList.add('notification');
+  block.classList.add(type);
+  blockTitle.classList.add('title');
+  blockTitle.innerText = title;
+  blockText.innerText = description;
+  block.append(blockTitle);
+  block.append(blockText);
+  document.body.append(block);
+
+  setTimeout(function() {
+    block.remove();
+  }, 2000);
+}
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(175, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(312, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
