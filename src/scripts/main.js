a0be882046379e4549cd4c0762ea4c5cf64dc890
Nigga <yomzfk@gmail.com>
2023-02-14 14:49:43 +0200

    Notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,32 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const fixDescription = description.split('\n').join('<br>');
+  const message = `
+    <div
+      class="notification ${type}"
+      style="
+        box-sizing: content-box;
+        top: ${posTop}px;
+        right: ${posRight}px;"
+    >
+      <h2 class="title">
+        ${title}
+      </h2>
+
+      <p>
+        ${fixDescription}
+      </p>
+    </div>
+  `;
+
+  const bodyLastEl = document.body.lastElementChild;
+
+  bodyLastEl.insertAdjacentHTML('beforebegin', message);
+
+  setTimeout(() => {
+    document.querySelector(`.${type}`).remove()
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
