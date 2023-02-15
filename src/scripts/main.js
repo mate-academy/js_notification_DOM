1faea2ef9a5d168444fbd935c5d8a21b53fb41fa
Oleksandr <98667890az@gmail.com>
2023-02-15 21:39:56 +0200

    notification DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  const notification = document.createElement('div')
+  const notification = document.createElement('div');
+
   notification.classList.add('notification', type);
   notification.style.top = `${posTop}px`;
   notification.style.right = `${posRight}px`;
 
   const titleOfNotification = document.createElement('h2');
+
   titleOfNotification.classList.add('title');
   titleOfNotification.textContent = title;
 
   const message = document.createElement('p');
+
   message.textContent = description;
 
-  notification.append(titleOfNotification,message);
+  notification.append(titleOfNotification, message);
 
   document.body.append(notification);
 
-  // setTimeout(() => {
-  //   document.body.remove(notification)
-  // }, 2000);
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
