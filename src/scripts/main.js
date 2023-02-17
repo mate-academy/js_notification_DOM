1202292d4fb72ae10126c626bf6ac2ba962fd5f2
mykolachupyak <chupyak@gmail.com>
2023-02-17 19:17:48 +0200

    improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,20 +2,21 @@
 
 const content = document.querySelector('body');
 
+// eslint-disable-next-line max-len
 const pushNotification = (posTop, posRight, title, description, type) => {
   const element = document.createElement('div');
 
   element.classList.add('notification');
   element.classList.add(type);
-  element.style.marginTop = posTop + 'px';
-  element.style.marginRight = posRight + 'px';
+  element.style.top = posTop + 'px';
+  element.style.right = posRight + 'px';
 
   element.innerHTML = `
     <h2 class="title">${title}</h2>
     <p>${description}</p>
   `;
   content.append(element);
-  setTimeout(removing, 3000);
+  setTimeout(removing, 2000);
 
   function removing() {
     content.children[2].remove();
@@ -24,12 +25,12 @@ const pushNotification = (posTop, posRight, title, description, type) => {
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+  + 'Notification should contain title and description.', 'success', content);
 
 pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+  + 'Notification should contain title and description.', 'error', content);
 
 pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+  + 'Notification should contain title and description.', 'warning', content);
