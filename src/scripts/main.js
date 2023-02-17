99be57658d7adb91fdcb65c6318761dec00b77b4
mykolachupyak <chupyak@gmail.com>
2023-02-17 19:26:30 +0200

    improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,12 +25,12 @@ const pushNotification = (posTop, posRight, title, description, type) => {
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'success', content);
+  + 'Notification should contain title and description.', 'success');
 
 pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'error', content);
+  + 'Notification should contain title and description.', 'error');
 
 pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'warning', content);
+  + 'Notification should contain title and description.', 'warning');
