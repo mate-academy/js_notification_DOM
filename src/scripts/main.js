affc59543efccc2efd8b391a9bd47e4737b0da12
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-21 11:08:43 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,30 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const divMassege = document.createElement('div');
+  const titleMassege = document.createElement('h2');
+
+  divMassege.className = 'notification ';
+  divMassege.classList.add(type);
+
+  body.append(divMassege);
+  body.style.position = 'relative';
+  divMassege.style.position = 'absolute';
+  divMassege.style.top = `${posTop}px`;
+  divMassege.style.right = `${posRight}px`;
+
+  titleMassege.className = 'title';
+  titleMassege.textContent = title;
+
+  divMassege.append(titleMassege);
+
+  const descriptMassege = document.createElement('p');
+
+  descriptMassege.textContent = description;
+
+  divMassege.append(descriptMassege);
+
 };
 
 pushNotification(10, 10, 'Title of Success message',
