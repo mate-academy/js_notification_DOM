b35641f1c69e663cd141f1926be60e7e58569a26
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-05 21:46:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,22 @@
 'use strict';
 
-// Объявление функции pushNotification с четырьмя параметрами
-// posTop и posRight - координаты уведомления на экране
-// title - заголовок уведомления
-// description - сообщение уведомления
-// type - тип уведомления (например, 'success', 'warning', 'error')
-
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // Создание нового элемента div
   const notification = document.createElement('div');
 
-  // Добавление классов 'notification' и типа уведомления к элементу
   notification.classList.add('notification');
   notification.classList.add(type);
 
-  // Вставка заголовка и сообщения уведомления в элемент
   notification.innerHTML = `
     <h2 class='title'>${title}</h2>
     <p>${description}</p>
   `;
 
-  // Установка позиции уведомления на экране
   notification.style.right = `${posRight}px`;
   notification.style.top = `${posTop}px`;
 
-   // Добавление элемента уведомления на страницу
-   document.body.appendChild(notification);
+  document.body.appendChild(notification);
 
-   // Автоматическое скрытие уведомления через 5 секунд
-   setTimeout(() => {
+  setTimeout(() => {
     notification.remove();
   }, 2000);
 };
