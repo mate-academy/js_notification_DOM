d3f9725d3bd0a8e7ee7f4c53099f84636878ee07
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-08 13:13:15 +0200

    Crearte, append and remove elements with javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,61 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+
+  body.insertAdjacentHTML('beforeend', `
+  <div class="notification ${type}">
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  </div>`);
+
+  const container = document.querySelector(`.notification.${type}`);
+
+  container.style.top = `${posTop}px`;
+  container.style.right = `${posRight}px`;
+
+  setTimeout(() => {
+    container.style.display = 'none';
+  }, 2000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+const titleOfSuccessMessage = 'Happy to inform you';
+const successMessage = 'Your order is accepted';
+const successMessagePosTop = 10;
+const successMessageClass = 'success';
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+const titleOfErrorMessage = 'Error';
+const errorMessage = 'That was last try. It can`t be helped ;)';
+const errorMessagePosTop = 150;
+const errorMessageClass = 'error';
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+const titleOfWarningMessage = 'Attention!';
+const warningMessage = 'Something is going on. Be carefull';
+const warningMessagePosTop = 290;
+const warningMessageClass = 'warning';
+
+const rightPositionOfMessage = 10;
+
+pushNotification(
+  successMessagePosTop,
+  rightPositionOfMessage,
+  titleOfSuccessMessage,
+  successMessage,
+  successMessageClass
+);
+
+pushNotification(
+  errorMessagePosTop,
+  rightPositionOfMessage,
+  titleOfErrorMessage,
+  errorMessage,
+  errorMessageClass
+);
+
+pushNotification(
+  warningMessagePosTop,
+  rightPositionOfMessage,
+  titleOfWarningMessage,
+  warningMessage,
+  warningMessageClass
+);
