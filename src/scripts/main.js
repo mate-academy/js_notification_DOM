897357f55436c9f6e315a9812ae7bf252a0b2220
Ilia Osoben <osoben17@gmail.com>
2023-03-16 16:46:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,22 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  document.body.append(div);
+  div.className = `notification  ${type}`;
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+  div.append(h2, p);
+
+  h2.className = 'title';
+  h2.textContent = title;
+
+  p.textContent = description;
+
+  setTimeout(() => div.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
