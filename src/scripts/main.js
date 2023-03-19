34a04c67df55584a89edcb320012eead9b6525ea
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-03-19 13:01:11 +0200

    Notification DOM v.0.1 - Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
-const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+const pushNotification = (posTop, posRight, title, description, type = '') => {
+  const blockElement = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const paragraph = document.createElement('p');
+
+  blockElement.classList.value = `notification ${type}`;
+  blockElement.style.top = `${posTop}px`;
+  blockElement.style.right = `${posRight}px`;
+
+  messageTitle.classList.add('title');
+  messageTitle.innerText = title;
+  messageTitle.style.fontSize = '18px';
+
+  paragraph.innerText = description;
+
+  blockElement.append(messageTitle, paragraph);
+  document.body.append(blockElement);
+  setTimeout(() => blockElement.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
