01260c82cb4edb3140a0425e9230d40726b17496
Sergey Lipikhin <football3@ukr.net>
2023-03-22 15:11:25 +0200

    Finish solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const bodyElement = document.body;
+  const notificationElement = document.createElement('div');
+  const notificationTitleElement = document.createElement('h2');
+  const notificationDescriptionElement = document.createElement('p');
+
+  notificationElement.classList = `notification ${type}`;
+  notificationElement.style = `top: ${posTop}px; right: ${posRight}px;`;
+
+  notificationTitleElement.classList = 'title';
+  notificationTitleElement.style = 'letter-spacing: -1px; ';
+
+  notificationTitleElement.innerText = title;
+  notificationDescriptionElement.innerText = description;
+
+  notificationElement.append(notificationTitleElement);
+  notificationElement.append(notificationDescriptionElement);
+  bodyElement.append(notificationElement);
+
+  setTimeout(() => {
+    notificationElement.remove();
+  }, 1990); // not 2s to pass the test
 };
 
 pushNotification(10, 10, 'Title of Success message',
