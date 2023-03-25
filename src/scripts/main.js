f3d796a8284bbc76a0f15dd43067911d74770a9b
Oleksandr Plias <pls777@ukr.net>
2023-03-25 03:02:05 +0200

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,41 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  const body = document.querySelector('body');
-  const message = document.createElement('div');
+  // const body = document.querySelector('body');
+  // const error = document.createElement('div');
 
-  message.classList.add('notification', `${type}`);
+  // error.classList.add('notification', `${type}`);
+  // error.style.top = `${posTop}px`;
+  // error.style.right = `${posRight}px`;
+
+  // error.insertAdjacentHTML('beforeend', `
+  //   <h2 class="title">
+  //     ${title}
+  //   </h2>
+  //   <p>
+  //     ${description}
+  //   </p>
+  // `);
+
+  // body.append(error);
+
+  // setTimeout(
+  //   () => error.remove(),
+  //   2000);
+
+  document.body.insertAdjacentHTML(`beforeend`, `
+    <div class = 'notification ${type}'>
+      <h2 class = 'title'>${title}</h2>
+      <p>${description}</p>
+    </div>
+  `);
+
+  const message = document.querySelector(`.${type}`);
+
   message.style.top = `${posTop}px`;
   message.style.right = `${posRight}px`;
 
-  message.insertAdjacentHTML('beforeend', `
-    <h2 class="title">
-      ${title}
-    </h2>
-    <p>
-      ${description}
-    </p>
-  `);
-
-  body.append(message);
-
-  setTimeout(
-    () => message.remove(),
-    2000);
+  setTimeout(() => message.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
