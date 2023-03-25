d890a32c1b2902936de09115b10d0988d6672aec
Oleksandr Plias <pls777@ukr.net>
2023-03-25 02:40:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const message = document.createElement('div');
+
+  message.classList.add('notification', `${type}`);
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  message.insertAdjacentHTML('beforeend', `
+    <h2 class="title">
+      ${title}
+    </h2>
+    <p>
+      ${description}
+    </p>
+  `);
+
+  body.append(message);
+
+  setTimeout(
+    () => message.remove(),
+    2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
