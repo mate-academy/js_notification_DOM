253a10ff5ab9b44f9907f60dc8df3c640a663dda
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-03-30 14:45:41 +0300

    Notification was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,36 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notif = document.createElement('div');
+  const titleEl = document.createElement('h2');
+  const descriptionEl = document.createElement('p');
+
+  titleEl.textContent = title;
+  descriptionEl.textContent = description;
+  notif.style.top = `${posTop}px`;
+  notif.style.right = `${posRight}px`;
+  titleEl.className = 'title';
+  notif.className = 'notification';
+
+  if (type === 'success') {
+    notif.className += ' success';
+  }
+
+  if (type === 'error') {
+    notif.className += ' error';
+  }
+
+  if (type === 'warning') {
+    notif.className += ' warning';
+  }
+
+  notif.append(titleEl);
+  notif.append(descriptionEl);
+  document.body.append(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
