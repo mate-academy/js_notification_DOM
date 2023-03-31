d4631a5c8ad469b616026ff75fba4475ed505358
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-03-31 20:21:07 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,49 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+
+  for (let i = 0; i < type[i].length; i++) {
+    if (type === 'success') {
+      helper(type[0]);
+    }
+
+    if (type === 'error') {
+      helper(type[1]);
+    }
+
+    if (type === 'warning') {
+      helper(type[2]);
+    }
+  }
+
+  function helper() {
+    const element = document.createElement('div');
+
+    element.classList = `notification ${type}`;
+
+    const h2 = document.createElement('h2');
+
+    h2.className = 'title';
+    h2.textContent = title;
+    element.append(h2);
+
+    const p = document.createElement('p');
+
+    p.textContent = description;
+    element.append(p);
+
+    body.append(element);
+
+    element.style.position = 'absolute';
+    element.style.top = `${posTop}px`;
+    element.style.right = `${posRight}px`;
+    element.style.width = '400px';
+
+    setTimeout(() => {
+      element.remove();
+    }, 2000);
+  }
 };
 
 pushNotification(10, 10, 'Title of Success message',
