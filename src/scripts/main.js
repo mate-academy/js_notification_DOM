a92a3f69daf04593caeed66b9720162c253ae881
Denys <kolbasin1990@gmail.com>
2023-04-06 00:20:09 +0300

    js_notification_DOM_v_01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,44 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  notification.classList.add('notification', type);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+  notification.style.width = `auto`;
+
+  const notificationTitle = document.createElement('h2');
+  notificationTitle.classList.add('title');
+  notificationTitle.textContent = title;
+
+  const notificationDescription = document.createElement('p');
+  notificationDescription.classList.add('p');
+  notificationDescription.textContent = description;
+  notificationDescription.innerHTML = description.replace(/\n/g, '<br>');
+
+  notification.appendChild(notificationTitle);
+  notification.appendChild(notificationDescription);
+  document.body.appendChild(notification);
+
+  const allNotifications = document.querySelectorAll('.notification');
+  const maxWidth = Math.max(...[...allNotifications].map((notification) => notification.offsetWidth));
+  allNotifications.forEach((notification) => notification.style.width = `${maxWidth}px`);
+
+  setTimeout(() => {
+    const notificationElement = document.querySelector('.notification');
+    notificationElement.remove();
+  }, 2000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+let posTop = 10;
+const posRight = 10;
+const gap = 10;
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+pushNotification(posTop, posRight, 'Title of Success message', 'Message example.\nNotification should contain title and \ndescription.', 'success');
+posTop += document.querySelector('.notification.success').offsetHeight + gap;
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+pushNotification(posTop, posRight, 'Title of Error message', 'Message example.\n Notification should contain title and \ndescription.', 'error');
+posTop += document.querySelector('.notification.error').offsetHeight + gap;
+
+pushNotification(posTop, posRight, 'Title of Warning message', 'Message example.\n Notification should contain title and \ndescription.', 'warning');
+posTop += document.querySelector('.notification.warning').offsetHeight + gap;
