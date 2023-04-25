b84815dffa07c826b0a0b08e28b1fb921bd978d1
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-04-26 00:12:22 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,14 @@
 const pushNotification = (posTop, posRight, title, description, type) => {
   const message = document.createElement('div');
   const messageTitle = document.createElement('h2');
-  const messageDescriprion = document.createElement('p');
+  const messageDescription = document.createElement('p');
 
   message.className = 'notification ' + type;
   messageTitle.className = 'title';
-  messageDescriprion.innerText = description;
+  messageDescription.innerText = description;
   messageTitle.innerText = title;
   message.append(messageTitle);
-  message.append(messageDescriprion);
+  message.append(messageDescription);
   document.querySelector('body').append(message);
   message.style.top = `${posTop}px`;
   message.style.right = `${posRight}px`;
@@ -22,10 +22,10 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-setTimeout(() => pushNotification(150, 10, 'Title of Error message',
+pushNotification(150, 10, 'Title of Error message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'error'), 2000);
+  + 'Notification should contain title and description.', 'error');
 
-setTimeout(() => pushNotification(290, 10, 'Title of Warning message',
+pushNotification(290, 10, 'Title of Warning message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'warning'), 4000);
+  + 'Notification should contain title and description.', 'warning');
