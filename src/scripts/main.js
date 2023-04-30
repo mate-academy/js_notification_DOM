2aa5f4a162c9e08a579ef2ce2fc7ac8b6225ab8b
Oksana Skuba <skuba.o.ua@gmail.com>
2023-04-30 17:05:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const divElement = document.createElement('div');
+  const h2Element = document.createElement('h2');
+  const pElement = document.createElement('p');
+
+  divElement.append(h2Element, pElement);
+  divElement.style.top = `${posTop}px`;
+  divElement.style.right = `${posRight}px`;
+  divElement.classList.add('notification', type);
+
+  document.body.append(divElement);
+
+  h2Element.innerText = title;
+  h2Element.classList.add('title');
+
+  pElement.innerText = description;
+
+  setTimeout(() => {
+    divElement.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
