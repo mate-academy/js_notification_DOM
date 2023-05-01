85bb7fa23da955a47916f94ee4c05c15ee5c3a28
Antonio S <soi4.toxa@ukr.net>
2023-05-01 10:17:03 +0300

    Notification-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notif = document.createElement('div');
+  const elTitle = document.createElement('h2');
+  const elDescript = document.createElement('p');
+
+  notif.className = `notification ${type}`;
+  elTitle.className = 'title';
+
+  elTitle.innerText = title;
+  elDescript.innerText = description;
+
+  notif.style.top = `${posTop}px`;
+  notif.style.right = `${posRight}px`;
+
+  notif.append(elTitle);
+  notif.append(elDescript);
+
+  document.body.append(notif);
+
+  setTimeout(() => notif.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
