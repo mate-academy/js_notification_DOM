c138477c8095c2eacd41af7976892471263ada73
Maria Shmakova <masha.shmakova@gmail.com>
2023-05-04 17:40:42 +0300

    notifications were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+
+  const block = document.createElement('div');
+  const header = document.createElement('h2');
+  const massage = document.createElement('p');
+
+  block.className = 'notification';
+  block.className += ` ${type}`;
+  block.style.top = `${posTop}px`;
+  block.style.right = `${posRight}px`;
+
+  header.textContent = title;
+  header.style.fontSize = '20px';
+
+  massage.textContent = description;
+  block.append(header);
+  block.append(massage);
+
+  document.body.append(block);
+
+  setTimeout(function() {
+    block.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
