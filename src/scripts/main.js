e5ec1bb7c69145ab7bd8697b2893ea72785fc1a8
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-05-05 20:16:57 +0200

    notifications was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,31 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div');
+
+  message.classList.add('notification');
+  message.classList.add(type);
+
+  message.innerHTML += `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+
+  message.style.cssText
+  = ` top: ${posTop}px; right: ${posRight};`;
+
+  document.body.append(message);
+
+  setTimeout(() => {
+    message.style.cssText += `display: none`;
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(165, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
