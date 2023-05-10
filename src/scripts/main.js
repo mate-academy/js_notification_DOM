35d4413a7b3abe8423cc996baec84e57b1c5c051
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-05-10 17:41:03 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
+  let topPosition = posTop;
+
   const notification = document.createElement('div');
 
   notification.classList.add('notification', type);
-  notification.style.marginTop = '10px';
 
   notification.innerHTML = `<h2 class="title">${title}</h2>
   <p>${description}</p>`;
@@ -12,18 +13,18 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const lastNotification = document.querySelector('.notification:last-of-type');
 
   if (lastNotification) {
-    notification.style.top = `${parseInt(lastNotification.style.top, 10)
-      + lastNotification.offsetHeight + 10}px`;
-  } else {
-    notification.style.top = `${posTop}px`;
+    topPosition = parseInt(lastNotification.style.top, 10)
+       + lastNotification.offsetHeight + 10;
   }
 
   Object.assign(notification.style, {
     position: 'absolute',
     right: `${posRight}px`,
-    top: `${notification.style.top}`,
+    top: `${topPosition}px`,
   });
+
   document.body.appendChild(notification);
+
   setTimeout(() => notification.remove(), 2000);
 };
 
