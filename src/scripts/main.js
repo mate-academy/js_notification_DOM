1beafd587a922bebfcb33e2b674f13d777f36329
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-05-10 13:05:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,30 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+  notification.style.marginTop = '10px';
+
+  notification.innerHTML = `<h2 class="title">${title}</h2>
+  <p>${description}</p>`;
+
+  const lastNotification = document.querySelector('.notification:last-of-type');
+
+  if (lastNotification) {
+    notification.style.top = `${parseInt(lastNotification.style.top, 10)
+      + lastNotification.offsetHeight + 10}px`;
+  } else {
+    notification.style.top = `${posTop}px`;
+  }
+
+  Object.assign(notification.style, {
+    position: 'absolute',
+    right: `${posRight}px`,
+    top: `${notification.style.top}`,
+  });
+  document.body.appendChild(notification);
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
