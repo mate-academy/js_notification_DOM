882d1f519905351ab55c55a2a3a1949d8fb40e41
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-17 17:21:55 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+
+  const block = document.createElement('div');
+
+  block.setAttribute('class', `notification ${type}`);
+  block.style.top = posTop + 'px';
+  block.style.right = posRight + 'px';
+
+  const h2 = document.createElement('h2');
+
+  h2.setAttribute('class', 'title');
+  h2.style.fontSize = '19px';
+  h2.innerText = title;
+
+  const p = document.createElement('p');
+  p.innerText = description;
+
+  block.append(h2);
+  block.append(p);
+  body.append(block);
+
+  setTimeout(() => block.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
