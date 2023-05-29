ff1568693501f6ee9e908262799370652d389231
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-29 22:12:28 +0300

    add final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,39 +4,53 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const mainElement = document.querySelector('body');
   const block = document.createElement('div');
 
-  block.classList.add('notification');
-  block.classList.add(`${type}`);
-  block.style.top = `${posTop}px`;
-  block.style.right = `${posRight}px`;
-  mainElement.append(block);
+  setTimeout(() => {
+    block.classList.add('notification');
+    block.classList.add(`${type}`);
+    block.style.top = `${posTop}px`;
+    block.style.right = `${posRight}px`;
+    mainElement.append(block);
 
-  const h2 = document.createElement('h2');
+    block.insertAdjacentHTML(
+      'beforeend',
+      `
+    <h2 class='title'>${title}</h2>
+    <p>${description}</p>
+  `
+    );
+  }, 2000);
 
-  h2.classList.add('title');
-  h2.innerText = title;
-  h2.style.whiteSpace = 'nowrap';
-  block.append(h2);
-
-  const p = document.createElement('p');
-
-  p.innerText = description;
-  block.append(p);
+  setTimeout(() => {
+    block.remove();
+  }, 4000);
 };
 
-setTimeout(() => {
-  pushNotification(10, 10, 'Title of Success message',
-    'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
-}, 2000);
+// setTimeout(() => {
+pushNotification(
+  10,
+  10,
+  'Title of Success message',
+  'Message example.\n ' + 'Notification should contain title and description.',
+  'success'
+);
+// }, 2000);
 
-setTimeout(() => {
-  pushNotification(150, 10, 'Title of Error message',
-    'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
-}, 2000);
+// setTimeout(() => {
+pushNotification(
+  150,
+  10,
+  'Title of Error message',
+  'Message example.\n ' + 'Notification should contain title and description.',
+  'error'
+);
+// }, 2000);
 
-setTimeout(() => {
-  pushNotification(290, 10, 'Title of Warning message',
-    'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
-}, 2000);
+// setTimeout(() => {
+pushNotification(
+  290,
+  10,
+  'Title of Warning message',
+  'Message example.\n ' + 'Notification should contain title and description.',
+  'warning'
+);
+// }, 2000);
