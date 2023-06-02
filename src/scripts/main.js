cb358aec66d0b651a5ecb49d9cd9fdf70224f606
Igor Galkin <igor_galkin@ukr.net>
2023-06-02 18:54:48 +0300

    1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  notification.classList.add('notification', type);
+  h2.classList.add('title');
+
+  h2.innerText = title;
+  p.innerText = description;
+
+  body.append(notification);
+  notification.appendChild(h2);
+  notification.appendChild(p);
+
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
