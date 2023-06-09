dfcdcda553503953b3cd88c572a49a9fd5feb59c
Vo7kov <jegor.volkov.a@gmail.com>
2023-06-09 13:40:00 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,7 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const notificationTitle = document.createElement('h2');
   const notificationText = document.createElement('p');
 
-  switch (type) {
-    case 'success': {
-      notificationBody.classList.add('success');
-      break;
-    }
-
-    case 'warning': {
-      notificationBody.classList.add('warning');
-      break;
-    }
-
-    case 'error': {
-      notificationBody.classList.add('error');
-      break;
-    }
-  }
-
-  notificationBody.classList.add('notification');
+  notificationBody.classList.add('notification', type);
   notificationBody.style.top = `${posTop}px`;
   notificationBody.style.right = `${posRight}px`;
 
@@ -41,10 +24,10 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(180, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(320, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
