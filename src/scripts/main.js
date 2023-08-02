da471baf73f2043ad906373227e8b01a4796b341
Artem Velychko <avelychko@icloud.com>
2023-08-02 16:04:59 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,41 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification');
+
+  switch (type) {
+    case 'success':
+      notification.classList.add('success');
+      break;
+    case 'error':
+      notification.classList.add('error');
+      break;
+    case 'warning':
+      notification.classList.add('warning');
+      break;
+  }
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  const titleContext = document.createElement('h2');
+
+  titleContext.classList.add('title');
+
+  const descriptionContext = document.createElement('p');
+
+  titleContext.innerText = title;
+  descriptionContext.innerText = description;
+
+  notification.appendChild(titleContext);
+  notification.appendChild(descriptionContext);
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
