7451166ad49e16bd7681cf75ec7b6dda547907d9
Andrii Khomiak <andr.hamster@gmail.com>
2023-09-02 14:25:48 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,42 @@
 'use strict';
 
+function getBackgroundColor(typeOfMessage) {
+  switch (typeOfMessage) {
+    case 'success':
+      return '#c0ddb6';
+
+    case 'error':
+      return '#ecb5b1';
+
+    case 'warning':
+      return '#f1e5bf';
+  }
+}
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const styles = notification.style;
+
+  styles.position = 'fixed';
+  styles.top = posTop + 'px';
+  styles.right = posRight + 'px';
+  styles.paddingInline = '10px';
+  styles.maxWidth = '300px';
+  styles.borderRadius = '10px';
+  styles.backgroundColor = getBackgroundColor(type);
+
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  h2.innerText = title;
+  p.innerText = description;
+
+  notification.append(h2, p);
+  document.body.append(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
