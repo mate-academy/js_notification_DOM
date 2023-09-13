8f1f151d3932a69ce658a73c99c72f13bc1404a9
Eugene Kulish <genekulish13@gmail.com>
2023-09-13 19:12:13 +0300

    solve the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notificationElement = document.createElement('div');
+  const bodyElement = document.querySelector('body');
+
+  const titleElement = document.createElement('h2');
+  const descriptionElement = document.createElement('p');
+
+  notificationElement.classList.add('notification');
+  titleElement.classList.add('title');
+  titleElement.classList.add(type);
+
+  titleElement.textContent = title;
+  descriptionElement.textContent = description;
+
+  notificationElement.style.top = `${posTop}px`;
+  notificationElement.style.right = `${posRight}px`;
+
+  notificationElement.append(titleElement, descriptionElement);
+  bodyElement.append(notificationElement);
+
+  setTimeout(() => {
+    bodyElement.removeChild(notificationElement);
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
