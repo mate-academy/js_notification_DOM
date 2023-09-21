6a03f8d463ef2070335dac1e2e5292fd1a7d5f5c
unknown <fanton81@gmail.com>
2023-09-21 19:05:20 +0300

    mySolution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div');
+  const header = document.createElement('h2');
+  const paragraph = document.createElement('p');
+
+  header.textContent = title;
+  header.className = 'title';
+  paragraph.textContent = description;
+
+  message.classList.add('notification', type);
+  
+
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+  header.style.fontSize = '16px';
+
+  message.append(header, paragraph);
+  document.body.append(message);
+
+  setTimeout(() => {
+    message.remove();
+  }, 2000);
 };
+
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
