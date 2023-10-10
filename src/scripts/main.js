3049797aa9a02f42e1eff7288765ec57821d54cb
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-10-10 14:46:56 +0300

    Finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  h2.textContent = title;
+  h2.classList.add('title');
+
+  p.textContent = description;
+  p.style.margin = '0px';
+
+  notification.classList.add('notification', `${type}`);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  notification.appendChild(h2);
+  notification.appendChild(p);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.parentNode.removeChild(notification);
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
