5bdd4cc4e8310441419fd2c7ed4d20417e7ecd0c
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-11 20:37:14 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,47 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+
+  div.classList.add('notification');
+
+  if (type === `warning`) {
+    div.classList.add('warning');
+    div.style.top = posTop + 'px';
+    div.style.right = posRight + 'px';
+
+    div.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+    `;
+  }
+
+  if (type === `error`) {
+    div.classList.add('error');
+    div.style.top = posTop + 'px';
+    div.style.right = posRight + 'px';
+
+    div.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+    `;
+  }
+
+  if (type === `success`) {
+    div.classList.add('success');
+
+    div.innerHTML = `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+    `;
+  }
+
+  body.append(div);
+
+  setTimeout(() => {
+    div.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
