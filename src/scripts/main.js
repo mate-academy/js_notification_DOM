83b91de5b92c8994bac99e8cee4d2e8f959c10c8
Юрий Мисюра <apriory13@meta.ua>
2023-12-05 14:28:01 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,48 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const elementBox = document.createElement('div');
+
+  elementBox.classList.add('notification', type);
+  elementBox.style.top = `${posTop}px`;
+  elementBox.style.right = `${posRight}px`;
+
+  elementBox.innerHTML += `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `;
+
+  document.body.appendChild(elementBox);
+
+  setTimeout(() => {
+    document.body.removeChild(elementBox);
+  }, 2000);
 };
 
-pushNotification(10, 10, 'Title of Success message',
+pushNotification(
+  10,
+  10,
+  'Title of Success message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+  + 'Notification should contain title and description.',
+  'success',
+);
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(
+  150,
+  10,
+  'Title of Error message',
   'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+  + 'Notification should contain title and description.',
+  'error',
+);
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(
+  290,
+  10,
+  'Title of Warning message',
+
   'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+  + 'Notification should contain title and description.',
+  'warning',
+);
