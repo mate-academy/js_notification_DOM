6ef92f5c4777c34d68906c1c44223a47516bace3
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-02-10 17:09:01 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,48 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable comma-dangle */
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notificationPosition = `top: ${posTop}px; right: ${posRight}px;`;
+  const notification =
+    `<div class='notification ${type}' style='${notificationPosition}'>` +
+    `<h3>${title}</h3>` +
+    `<p>${description}</p>` +
+    `</div>`;
+
+  document.body.innerHTML += notification;
 };
 
-pushNotification(10, 10, 'Title of Success message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'success');
+const removeNotification = () => {
+  const notifications = document.querySelectorAll(`.notification`);
+  let time = null;
 
-pushNotification(150, 10, 'Title of Error message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'error');
+  clearTimeout(time);
+  time = setTimeout(() => notifications.forEach((item) => item.remove()), 2000);
+};
 
-pushNotification(290, 10, 'Title of Warning message',
-  'Message example.\n '
-  + 'Notification should contain title and description.', 'warning');
+pushNotification(
+  10,
+  10,
+  'Title of Success message',
+  'Message example.\n ' + 'Notification should contain title and description.',
+  'success'
+);
+
+pushNotification(
+  150,
+  10,
+  'Title of Error message',
+  'Message example.\n ' + 'Notification should contain title and description.',
+  'error'
+);
+
+pushNotification(
+  290,
+  10,
+  'Title of Warning message',
+  'Message example.\n ' + 'Notification should contain title and description.',
+  'warning'
+);
+
+removeNotification();
