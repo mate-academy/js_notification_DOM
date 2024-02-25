ad0e614bfea7eb5307e24449ea3633cfc508abed
Dmytro Torop <young-z@i.ua>
2024-02-25 14:14:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,32 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.textContent = title;
+  notificationTitle.classList.add('title');
+
+  const notificationText = document.createElement('p');
+
+  notificationText.textContent = description;
+
+  notification.append(notificationTitle, notificationText);
+  document.body.appendChild(notification);
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(160, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
