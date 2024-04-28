79430ea1f92ddef7374b24cfc0d45528c5f2379f
Volodymyr Shepel <web.shepel@gmail.com>
2024-04-28 17:11:47 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="notification ${type}">
+      <h2 class="title">${title}</h2>
+      <p>${description}</p>
+    </div>
+  `,
+  );
+
+  const notification = document.querySelector('.notification:last-child');
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  setTimeout(() => {
+    notification.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
