1bc25b0deb411c430242ca1ca1a6d06cd50df117
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-04-28 11:02:19 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const newMessage = document.createElement('div');
+
+  newMessage.classList.add('notification', type);
+  newMessage.style.top = `${posTop}px`;
+  newMessage.style.right = `${posRight}px`;
+
+  const messageTitle = document.createElement('h2');
+
+  messageTitle.classList.add('title');
+  messageTitle.textContent = title;
+
+  const messageDescription = document.createElement('p');
+
+  messageDescription.textContent = `${description}`;
+
+  newMessage.appendChild(messageTitle);
+  newMessage.appendChild(messageDescription);
+  document.body.appendChild(newMessage);
+
+  setTimeout(() => newMessage.remove(), 2000);
 };
 
 pushNotification(
