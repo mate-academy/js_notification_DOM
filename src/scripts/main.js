9d97de97212297100a22006b4a8edaa4bce7a551
zozulia.kirill <kirillzozulya95@gmail.com>
2024-05-29 17:11:44 +0300

    resolve task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,29 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+  notification.style.position = 'absolute';
+  notification.style.right = `${posRight}px`;
+  notification.style.top = `${posTop}px`;
+
+  const titleElement = document.createElement('h2');
+
+  titleElement.className = 'title';
+  titleElement.textContent = title;
+  notification.appendChild(titleElement);
+
+  const descriptionElement = document.createElement('p');
+
+  descriptionElement.textContent = description;
+  notification.appendChild(descriptionElement);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    document.body.removeChild(notification);
+  }, 2000);
 };
 
 pushNotification(
@@ -13,7 +35,7 @@ pushNotification(
 );
 
 pushNotification(
-  150,
+  165,
   10,
   'Title of Error message',
   'Message example.\n ' + 'Notification should contain title and description.',
