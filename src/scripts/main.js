8bf2d37f56b362e53864c88938c84f363d814f51
Yevhenii <polotnyak2004@gmail.com>
2024-06-05 14:22:23 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const tit = document.createElement('h2');
+  const desc = document.createElement('p');
+
+  notification.classList.add(type, 'notification');
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  tit.innerText = title;
+  tit.classList.add('title');
+  desc.innerText = description;
+
+  notification.append(tit);
+  notification.append(desc);
+  document.body.prepend(notification);
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(
