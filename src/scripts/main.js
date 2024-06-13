c474edc6e40031fab00ac974ed4e45dccd611a93
Oleksandr Lytovchenko <jaster93him@gmail.com>
2024-06-13 19:12:18 +0300

    notification added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const container = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const message = document.createElement('p');
+
+  container.classList.add('notification', type);
+  container.style.top = `${posTop}px`;
+  container.style.right = `${posRight}px`;
+
+  messageTitle.classList.add('title');
+  messageTitle.innerText = title;
+  message.innerText = description;
+
+  container.append(messageTitle, message);
+  body.append(container);
+
+  setTimeout(() => {
+    container.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
