50c623c4b874e852ce0f4b3d981242f1383a8639
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-17 14:42:42 +0200

    add notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,42 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  // creating div, h2 and p
+  const message = document.createElement('div');
+  const titleElement = document.createElement('h2');
+  const descriptionElement = document.createElement('p');
+
+  // setting styles and classnames for notification
+  message.classList.add('notification', type);
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+  message.style.fontFamily = 'inherit';
+  message.style.width = 'auto';
+
+  // setting styles and classname for title
+  titleElement.className = 'title';
+  titleElement.textContent = title;
+  descriptionElement.textContent = description;
+
+  // setting background color for notification
+  if (type === 'success') {
+    message.style.backgroundColor = '#ACE1AF';
+  } else if (type === 'error') {
+    message.style.backgroundColor = '#fd5c63';
+  } else if (type === 'warning') {
+    message.style.backgroundColor = '#F0E68C';
+  }
+
+  // nesting
+  message.appendChild(titleElement);
+  message.appendChild(descriptionElement);
+
+  // appending message on a website
+  document.body.appendChild(message);
+
+  setTimeout(() => {
+    message.remove();
+  }, 2000);
 };
 
 pushNotification(
