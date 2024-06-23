f44604daab3b2db1e14e0da5336d6196d030c218
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-23 16:35:46 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,48 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const div = document.createElement('div');
+
+  div.classList.add('notification');
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  const h2 = document.createElement('h2');
+
+  h2.textContent = title;
+  h2.classList.add('title');
+
+  const p = document.createElement('p');
+
+  p.textContent = description;
+
+  switch (type) {
+    case 'success':
+      div.classList.add('success');
+      break;
+
+    case 'warning':
+      div.classList.add('warning');
+      break;
+
+    case 'error':
+      div.classList.add('error');
+      break;
+
+    default:
+      break;
+  }
+
+  div.appendChild(h2);
+  div.appendChild(p);
+  body.appendChild(div);
+
+  setTimeout(() => {
+    body.removeChild(div);
+  }, 2000);
 };
 
 pushNotification(
