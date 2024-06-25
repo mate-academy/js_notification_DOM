1d7e1952605fb1f2d677663874c6e28514675a53
4xmplme <andrii_kondratenko@icloud.com>
2024-06-25 22:33:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const notificationTitle = document.createElement('h2');
+  const notificationParagraph = document.createElement('p');
+
+  notification.classList.add('notification', type);
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  notificationTitle.classList.add('title');
+  notificationTitle.innerText = title;
+
+  notificationParagraph.innerText = description;
+
+  notification.append(notificationTitle, notificationParagraph);
+
+  document.body.append(notification);
+
+  setTimeout(() => (notification.style.display = 'none'), 2000);
 };
 
 pushNotification(
