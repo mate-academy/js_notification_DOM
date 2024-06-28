e1e3bacefad67e772be9637d03a1ebbc8bdd0f5d
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-28 15:40:58 +0300

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,50 @@
 'use strict';
 
+const DONE = 'success';
+const ERROR = 'error';
+const WARNING = 'warning';
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.getElementsByTagName('body')[0];
+  const mainBlock = document.createElement('div');
+  const mainTitle = document.createElement('h4');
+  const mainDescription = document.createElement('p');
+
+  mainTitle.textContent = title;
+  mainTitle.style.marginBottom = '10px';
+  mainDescription.textContent = description;
+
+  mainBlock.appendChild(mainTitle);
+  mainBlock.appendChild(mainDescription);
+
+  mainBlock.style.position = 'absolute';
+  mainBlock.style.bottom = `${posTop}px`;
+  mainBlock.style.right = `${posRight}px`;
+  mainBlock.style.width = '300px';
+  mainBlock.style.height = '120px';
+  mainBlock.style.padding = '10px';
+  mainBlock.style.boxSizing = 'border-box';
+  mainBlock.style.borderRadius = '10px';
+
+  switch (type) {
+    case ERROR:
+      mainBlock.style.backgroundColor = 'red';
+      break;
+
+    case DONE:
+      mainBlock.style.backgroundColor = 'green';
+      break;
+
+    case WARNING:
+      mainBlock.style.backgroundColor = 'yellow';
+      break;
+  }
+
+  body.appendChild(mainBlock);
+
+  setTimeout(() => {
+    mainBlock.remove();
+  }, 2000);
 };
 
 pushNotification(
