055af90df1e4ffaf4f7ba7f127c4771b8890866e
beliyandr <beliyandr@gmail.com>
2024-06-28 20:38:25 +0300

    2-d variant

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,29 +16,43 @@ const pushNotification = (posTop, posRight, title, description, type) => {
 
   notificationBlock.insertAdjacentHTML('afterbegin', info);
   logo.insertAdjacentElement('afterend', notificationBlock);
+
+  setTimeout(() => {
+    notificationBlock.remove();
+  }, 2000)
+
+
 };
 
-pushNotification(
-  10,
-  10,
-  'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'success',
-);
+setTimeout(() => {
+  pushNotification(
+    10,
+    10,
+    'Title of Success message',
+    'Message example.\n ' +
+    'Notification should contain title and description.',
+    'success',
+  );
+}, 500);
 
-pushNotification(
-  150,
-  10,
-  'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'error',
-);
+setTimeout(() => {
+  pushNotification(
+    150,
+    10,
+    'Title of Error message',
+    'Message example.\n ' +
+    'Notification should contain title and description.',
+    'error',
+  );
+}, 1000);
 
-pushNotification(
-  290,
-  10,
-  'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'warning',
-);
-
+setTimeout(() => {
+  pushNotification(
+    290,
+    10,
+    'Title of Warning message',
+    'Message example.\n ' +
+    'Notification should contain title and description.',
+    'warning',
+  );
+}, 1500);
