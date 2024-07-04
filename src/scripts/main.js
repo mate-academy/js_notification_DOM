ce7bfdaa6d8b7f38880acdb87d3c23185dcdcb3d
HunchakAndrii <andreygunchak1@gmail.com>
2024-07-04 11:39:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageDescription = document.createElement('p');
+
+  message.classList.add('notification', type);
+  messageTitle.classList.add('title');
+
+  messageTitle.textContent = title;
+  messageDescription.textContent = description;
+
+  message.style.cssText = `position: fixed; top: ${posTop}px; right: ${posRight}px; width: 320px`;
+
+  messageDescription.innerHTML =
+    'Message example. <br> Notification should contain title and description.';
+
+  message.append(messageTitle, messageDescription);
+
+  document.body.append(message);
+
+  setTimeout(() => {
+    message.remove();
+  }, '2000');
 };
 
 pushNotification(
