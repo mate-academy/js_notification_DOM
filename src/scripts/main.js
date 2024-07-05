48d7d59075aa502679741ee722b329c5d3426236
Kirill <ovoo1339@gmail.com>
2024-07-05 13:04:39 +0300

    work2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,18 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const div = document.createElement('div');
 
   div.classList.add('notification', type);
-  div.style.position = 'absolute';
+  // div.style.position = 'absolute';
   div.style.top = `${posTop}px`;
   div.style.right = `${posRight}px`;
 
   const titles = document.createElement('h2');
+
   titles.className = 'title';
   titles.textContent = title;
   div.appendChild(titles);
 
   const descriptions = document.createElement('p');
+
   descriptions.textContent = description;
   div.appendChild(descriptions);
 
