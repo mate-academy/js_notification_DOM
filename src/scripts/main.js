b64ff0126d7bd9c19e74bc9ac78fd43878b7270b
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-06 10:34:54 +0300

    js_notification_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,52 @@
 'use strict';
 
+let notifications = [];
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+
+  notification.style.position = 'absolute';
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  const titleElement = document.createElement('h2');
+
+  titleElement.classList.add('title');
+  titleElement.textContent = title;
+
+  const descriptionElement = document.createElement('p');
+
+  descriptionElement.textContent = description;
+
+  notification.appendChild(titleElement);
+  notification.appendChild(descriptionElement);
+
+  document.body.appendChild(notification);
+
+  notifications.forEach((notif) => {
+    const currentTop = parseInt(notification.style.top);
+    const existingTop = parseInt(notif.style.top);
+
+    if (
+      currentTop >= existingTop &&
+      currentTop <= existingTop + notif.clientHeight + 20
+    ) {
+      notification.style.top = `${existingTop + notif.clientHeight + 20}px`;
+    }
+  });
+
+  notifications.push(notification);
+
+  setTimeout(() => {
+    notification.remove();
+    notifications = notifications.filter((notif) => notif !== notification);
+
+    notifications.forEach((notif, index) => {
+      notif.style.top = `${posTop + index * (notif.clientHeight + 20)}px`;
+    });
+  }, 2000);
 };
 
 pushNotification(
