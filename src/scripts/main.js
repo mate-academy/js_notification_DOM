b69a1b3463fcb4cbedf4a5b1f95ee9ac08c496a2
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2024-07-07 20:29:24 +0300

    display none

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,7 +22,7 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   document.body.appendChild(notification);
 
   setTimeout(() => {
-    notification.remove();
+    notification.style.display = 'none';
   }, 2000);
 };
 
