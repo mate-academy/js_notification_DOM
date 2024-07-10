685039c44e1049cb62776d3671de69a44a0bae40
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-10 11:32:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const notification = document.createElement('div');
+  const title1 = document.createElement('h2');
+  const description1 = document.createElement('p');
+
+  notification.classList.add('notification');
+  notification.classList.add(type);
+  title1.classList.add('title');
+  title1.innerText = title;
+  description1.innerHTML = description;
+  body.append(notification);
+  notification.append(title1);
+  notification.append(description1);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  setTimeout(() => {
+    notification.style.visibility = 'hidden';
+  }, 1800);
 };
+const element = document.querySelector('.logo');
+
+getComputedStyle(element);
 
 pushNotification(
   10,
