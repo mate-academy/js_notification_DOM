5e8123245bb680d2a19722de9d6e063906b23f8c
Your Name <ruslankurman13@gmail.com>
2024-07-24 17:04:26 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,54 @@
 'use strict';
 
-const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
-};
+document.addEventListener('DOMContentLoaded', () => {
+  const pushNotification = (posTop, posRight, title, description, type) => {
+    const notification = document.createElement('div');
 
-pushNotification(
-  10,
-  10,
-  'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'success',
-);
+    notification.classList.add('notification', type);
 
-pushNotification(
-  150,
-  10,
-  'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'error',
-);
+    const titleElement = document.createElement('h2');
 
-pushNotification(
-  290,
-  10,
-  'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'warning',
-);
+    titleElement.classList.add('title');
+    titleElement.textContent = title;
+
+    const descriptionElement = document.createElement('p');
+
+    descriptionElement.textContent = description;
+
+    notification.appendChild(titleElement);
+    notification.appendChild(descriptionElement);
+
+    notification.style.position = 'absolute';
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+
+    document.body.appendChild(notification);
+  };
+
+  pushNotification(
+    10,
+    10,
+    'Title of Success message',
+    'Message example.\n ' +
+      'Notification should contain title and description.',
+    'success',
+  );
+
+  pushNotification(
+    150,
+    10,
+    'Title of Error message',
+    'Message example.\n ' +
+      'Notification should contain title and description.',
+    'error',
+  );
+
+  pushNotification(
+    290,
+    10,
+    'Title of Warning message',
+    'Message example.\n ' +
+      'Notification should contain title and description.',
+    'warning',
+  );
+});
