6ccd024ed3efa96ef4f838457fc6b7691a7226b6
Oleksandra Kuziura <radioday2014@gmail.com>
2024-07-26 12:34:32 +0300

    finish notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,38 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  const innerH2 = document.createTextNode(title);
+  const innerP = document.createTextNode(description);
+
+  div.style.right = `${posRight}px`;
+  div.style.top = `${posTop}px`;
+  h2.setAttribute('class', 'title');
+  div.setAttribute('class', 'notification');
+  h2.appendChild(innerH2);
+  p.appendChild(innerP);
+  div.appendChild(h2);
+  div.appendChild(p);
+
+  document.body.append(div);
+
+  if (type === 'success') {
+    div.classList.add('success');
+  }
+
+  if (type === 'error') {
+    div.classList.add('error');
+  }
+
+  if (type === 'warning') {
+    div.classList.add('warning');
+  }
+
+  setTimeout(() => {
+    div.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
@@ -13,7 +44,7 @@ pushNotification(
 );
 
 pushNotification(
-  150,
+  165,
   10,
   'Title of Error message',
   'Message example.\n ' + 'Notification should contain title and description.',
