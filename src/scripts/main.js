9129864cccf33e4da9e26f563c9bfd915033e9ca
voidmain69 <photoreznichenko@gmail.com>
2024-08-10 19:25:38 +0300

    fix node version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  const titleElement = document.createElement('h2');
+
+  titleElement.classList.add('title');
+  titleElement.textContent = title;
+
+  const pElement = document.createElement('p');
+
+  pElement.innerText = description;
+
+  notification.appendChild(titleElement);
+  notification.appendChild(pElement);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => (notification.style.visibility = 'hidden'), 2000);
 };
 
 pushNotification(
