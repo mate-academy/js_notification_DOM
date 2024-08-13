0fe7639bd38ee1cd7c5a40f1519476e7a0799749
iryna lysko <goiralysko@gmail.com>
2024-08-13 16:43:51 +0300

    add removable notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,33 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const notificationTitle = document.createElement('h1');
+  const notificationDesc = document.createElement('p');
+
+  notification.className = 'notification';
+  notification.classList.add(type);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  notificationTitle.className = 'title';
+  notificationTitle.style.letterSpacing = '-1px';
+  notificationTitle.textContent = title;
+
+  notificationDesc.textContent = description;
+  notificationDesc.style.whiteSpace = 'pre-wrap';
+
+  notification.append(notificationTitle, notificationDesc);
+  document.body.append(notification);
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(
   10,
   10,
   'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.\n' + 'Notification should contain title and description.',
   'success',
 );
 
@@ -16,7 +35,7 @@ pushNotification(
   150,
   10,
   'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.\n' + 'Notification should contain title and description.',
   'error',
 );
 
@@ -24,6 +43,6 @@ pushNotification(
   290,
   10,
   'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.\n' + 'Notification should contain title and description.',
   'warning',
 );
