70db8fb385ad9c700b9a12b1c34ce2980022bbb2
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-08-15 15:25:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,40 @@
 'use strict';
 
+let previousNotificationBottom = 10;
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.body;
+  const message = document.createElement('div');
+  const messageTitle = document.createElement('h2');
+  const messageDescr = document.createElement('p');
+
+  message.classList.add('notification');
+  messageTitle.classList.add('title');
+
+  if (type === 'success') {
+    message.classList.add('success');
+  } else if (type === 'error') {
+    message.classList.add('error');
+  } else if (type === 'warning') {
+    message.classList.add('warning');
+  }
+
+  messageTitle.textContent = title;
+  messageDescr.textContent = description;
+
+  message.style.position = 'absolute';
+  message.style.top = `${previousNotificationBottom}px`;
+  message.style.right = `${posRight}px`;
+
+  message.appendChild(messageTitle);
+  message.appendChild(messageDescr);
+  body.appendChild(message);
+
+  previousNotificationBottom += message.offsetHeight + 10;
+
+  setTimeout(() => {
+    message.remove();
+  }, 2000);
 };
 
 pushNotification(
