cbeb2b598c59d717bd1f340cc72a84e4a504c036
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-08-15 22:14:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,20 @@
 'use strict';
 
-let previousNotificationBottom = 10;
-
 const pushNotification = (posTop, posRight, title, description, type) => {
-  const body = document.body;
   const message = document.createElement('div');
   const messageTitle = document.createElement('h2');
   const messageDescr = document.createElement('p');
 
-  message.classList.add('notification');
-  messageTitle.classList.add('title');
-
-  if (type === 'success') {
-    message.classList.add('success');
-  } else if (type === 'error') {
-    message.classList.add('error');
-  } else if (type === 'warning') {
-    message.classList.add('warning');
-  }
-
+  message.classList.add('notification', type);
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+  messageTitle.className = 'title';
   messageTitle.textContent = title;
   messageDescr.textContent = description;
-
-  message.style.position = 'absolute';
-  message.style.top = `${previousNotificationBottom}px`;
-  message.style.right = `${posRight}px`;
-
-  message.appendChild(messageTitle);
-  message.appendChild(messageDescr);
-  body.appendChild(message);
-
-  previousNotificationBottom += message.offsetHeight + 10;
-
-  setTimeout(() => {
-    message.remove();
-  }, 2000);
+  messageDescr.style.whiteSpace = 'pre-wrap';
+  message.append(messageTitle, messageDescr);
+  document.body.appendChild(message);
+  setTimeout(() => (message.style.visibility = 'hidden'), 2000);
 };
 
 pushNotification(
