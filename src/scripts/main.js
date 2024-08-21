32ff5124caa82ad48c04c6dc89772875e4dcefb4
Ivan <fanmetalheavy@gmail.com>
2024-08-21 17:02:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,26 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notification = document.createElement('div');
+  const notificationTitle = document.createElement('h2');
+  const notificationDescription = document.createElement('p');
+
+  notification.setAttribute('class', `notification ${type}`);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  notificationTitle.setAttribute('class', 'title');
+  notificationTitle.textContent = title;
+
+  notificationDescription.textContent = description;
+
+  notification.append(notificationTitle);
+  notification.append(notificationDescription);
+  document.body.append(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 2000);
 };
 
 pushNotification(
@@ -13,7 +33,7 @@ pushNotification(
 );
 
 pushNotification(
-  150,
+  161.19,
   10,
   'Title of Error message',
   'Message example.\n ' + 'Notification should contain title and description.',
@@ -21,7 +41,7 @@ pushNotification(
 );
 
 pushNotification(
-  290,
+  284.38,
   10,
   'Title of Warning message',
   'Message example.\n ' + 'Notification should contain title and description.',
