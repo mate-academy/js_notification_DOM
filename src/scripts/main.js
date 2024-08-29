41cc62eea6c0f691d95860234ae132590b570ba2
sinner1993 <sergio1993goga@gmail.com>
2024-08-29 19:19:54 +0200

    asdasddasd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,21 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const h2 = document.createElement('h2');
   const p = document.createElement('p');
   const message = document.createElement('div');
-  const body = document.querySelector('body');
 
   message.classList.add('notification', type);
   message.style.position = 'absolut';
   message.style.top = `${posTop}px`;
   message.style.right = `${posRight}px`;
   h2.textContent = title;
+  h2.className = 'title';
   p.textContent = description;
+  p.className = 'description';
   message.append(h2);
   message.append(p);
-  body.append(message);
+  document.body.appendChild(message);
 
   setTimeout(() => {
-    message.remove();
+    message.style.visibility = 'hidden';
   }, 2000);
 };
 
