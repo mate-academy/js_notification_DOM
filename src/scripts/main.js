3efb25d0f2ee0e1eae364bdf4ec22d2e24f5685c
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-30 16:19:55 +0300

    notification task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const messageBlock = document.createElement('div');
+  const blockTitle = document.createElement('h2');
+  const blockText = document.createElement('p');
+
+  document.body.append(messageBlock);
+  messageBlock.classList.add('notification', type);
+  messageBlock.append(blockTitle, blockText);
+  messageBlock.style.top = `${posTop}px`;
+  messageBlock.style.right = `${posRight}px`;
+  blockTitle.classList.add('title');
+  blockTitle.textContent = title;
+  blockText.textContent = description;
+
+  setTimeout(() => {
+    messageBlock.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
