706f482b637f340d76af06cff6482ce14d510f26
Liliia Danylenko <liliia.danylenko.dev@gmail.com>
2024-09-08 19:01:34 +0300

    fixed the spacing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,10 +27,10 @@ pushNotification(10, 10, 'Title of Success message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'success');
 
-pushNotification(150, 10, 'Title of Error message',
+pushNotification(160, 10, 'Title of Error message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'error');
 
-pushNotification(290, 10, 'Title of Warning message',
+pushNotification(283, 10, 'Title of Warning message',
   'Message example.\n '
   + 'Notification should contain title and description.', 'warning');
