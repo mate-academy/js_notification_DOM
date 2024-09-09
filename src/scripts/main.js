6fe154944ac03e9802763f0fed4b9489db6afb50
olshum <olshum8@gmail.com>
2024-09-09 20:17:48 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,41 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const element = document.createElement('div');
+
+  element.classList.add('notification');
+
+  switch (type) {
+    case 'success':
+      element.classList.add('success');
+      break;
+    case 'error':
+      element.classList.add('error');
+      break;
+    case 'warning':
+      element.classList.add('warning');
+      break;
+  }
+
+  element.style.top = ${posTop}px;
+  element.style.right = ${posRight}px;
+
+  const titleElement = document.createElement('h2');
+
+  titleElement.textContent = title;
+  element.appendChild(titleElement);
+  titleElement.classList.add('title');
+
+  const titleDesc = document.createElement('p');
+
+  titleDesc.textContent = description;
+  element.appendChild(titleDesc);
+
+  document.body.appendChild(element);
+
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
@@ -13,7 +47,7 @@ pushNotification(
 );
 
 pushNotification(
-  150,
+  165,
   10,
   'Title of Error message',
   'Message example.\n ' + 'Notification should contain title and description.',
