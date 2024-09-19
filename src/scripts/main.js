7a068f631c220679174a91b8c8ddb6e692895f83
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-19 08:43:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,38 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+  const header = document.createElement('h2');
+  const text = document.createElement('p');
+
+  block.setAttribute('class', `nonfication_${type}`);
+  header.setAttribute('class', title);
+  text.textContent = description;
+
+  nonfication(block, type, posTop, posRight);
+
+  document.body.append(block);
+  block.append(header, text);
+
+  setTimeout(() => {
+    block.remove();
+  }, 2000);
+
+  function nonfication(element, typeS, posTopS, posRightS) {
+    element.style.position = 'fixed';
+    element.style.top = `${posTop}vh`;
+    element.style.right = `${posRight}vh`;
+    element.style.borderRadius = '16px';
+    element.style.padding = '10px';
+
+    if (type === 'success') {
+      element.style.backgroundColor = 'green';
+    } else if (type === 'error') {
+      element.style.backgroundColor = 'yelow';
+    } else if (type === 'warning') {
+      element.style.backgroundColor = 'red';
+    }
+  }
 };
 
 pushNotification(
