d7084b76cb41951019b8258e3e69adb890f6353e
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-20 03:43:43 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,34 +5,30 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   const header = document.createElement('h2');
   const text = document.createElement('p');
 
-  block.setAttribute('class', `nonfication_${type}`);
+  block.setAttribute('class', `nonfication ${type}`);
   header.setAttribute('class', title);
   text.textContent = description;
-
-  nonfication(block, type, posTop, posRight);
 
   document.body.append(block);
   block.append(header, text);
 
-  setTimeout(() => {
-    block.remove();
-  }, 2000);
+  block.style.position = 'fixed';
+  block.style.top = `${posTop}vh`;
+  block.style.right = `${posRight}vh`;
+  block.style.borderRadius = '16px';
+  block.style.padding = '10px';
 
-  function nonfication(element, typeS, posTopS, posRightS) {
-    element.style.position = 'fixed';
-    element.style.top = `${posTop}vh`;
-    element.style.right = `${posRight}vh`;
-    element.style.borderRadius = '16px';
-    element.style.padding = '10px';
-
-    if (type === 'success') {
-      element.style.backgroundColor = 'green';
-    } else if (type === 'error') {
-      element.style.backgroundColor = 'yelow';
-    } else if (type === 'warning') {
-      element.style.backgroundColor = 'red';
-    }
+  if (type === 'success') {
+    block.style.backgroundColor = 'green';
+  } else if (type === 'error') {
+    block.style.backgroundColor = 'yelow';
+  } else if (type === 'warning') {
+    block.style.backgroundColor = 'red';
   }
+
+  setTimeout(() => {
+    block.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
