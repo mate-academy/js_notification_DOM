81a9b32685f1cd0ff43b612a3520adbf0d0f1d7c
Anastasiia-Tatarchenko <neveykina.nastya@gmail.com>
2024-09-25 22:42:23 +0200

    task_done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  h2.textContent = title;
+  p.textContent = description;
+
+  h2.classList.add('title');
+  div.classList.add('notification', type);
+
+  div.style.top = posTop + 'px';
+  div.style.right = posRight + 'px';
+
+  div.append(h2, p);
+  document.body.appendChild(div);
+
+  setTimeout(() => {
+    div.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
