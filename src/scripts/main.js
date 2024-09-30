25cd36259688c8e7f6fdfc2041bb71f590f89050
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-30 17:43:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const notificationBody = document.createElement('div');
+  const notificationTitle = document.createElement('h2');
+  const notificationDescription = document.createElement('p');
+
+  notificationBody.classList.add('notification', type);
+  notificationTitle.classList.add(`title`);
+
+  notificationTitle.textContent = title;
+  notificationDescription.textContent = description;
+
+  notificationBody.style.position = 'absolute';
+  notificationBody.style.top = `${posTop}px`;
+  notificationBody.style.right = `${posRight}px`;
+
+  body.prepend(notificationBody);
+  notificationBody.prepend(notificationTitle);
+  notificationBody.append(notificationDescription);
+
+  setTimeout(() => {
+    notificationBody.remove();
+  }, 2000);
 };
 
 pushNotification(
@@ -13,7 +34,7 @@ pushNotification(
 );
 
 pushNotification(
-  150,
+  160,
   10,
   'Title of Error message',
   'Message example.\n ' + 'Notification should contain title and description.',
@@ -21,7 +42,7 @@ pushNotification(
 );
 
 pushNotification(
-  290,
+  280,
   10,
   'Title of Warning message',
   'Message example.\n ' + 'Notification should contain title and description.',
