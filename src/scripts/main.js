ffca2a448ce4d8ec6d9e7c09c6b4f35dec02492f
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-09-30 10:42:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  div.classList.add('notification', type);
+  div.style.top = `${posTop}px`;
+  div.style.right = `${posRight}px`;
+
+  h2.classList.add('title');
+  h2.textContent = title;
+
+  p.textContent = description;
+
+  document.body.append(div);
+  div.append(h2);
+  div.append(p);
+
+  setTimeout(() => {
+    div.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
