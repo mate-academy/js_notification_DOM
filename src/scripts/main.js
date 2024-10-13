ab731852307368f46fe16f6512b214f1e1ce187c
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-10-13 18:37:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,35 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector("body")
+  const message = document.createElement("div")
+  message.classList.add('notification')
+  message.classList.add(type);
+
+
+  const titleElement = document.createElement('h2')
+  titleElement.classList.add('title')
+  titleElement.textContent = title;
+  message.appendChild(titleElement)
+
+
+  const descriptionElement = document.createElement('p')
+  descriptionElement.textContent = description;
+  message.appendChild(descriptionElement)
+
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  body.appendChild(message)
+
+  setTimeout(() => {
+    message.remove()
+
+  }, 2000)
+
+
+
+
 };
 
 pushNotification(
