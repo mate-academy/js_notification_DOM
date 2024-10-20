af9509ab324c6b432d29eeab6918f70ba249037b
Anna <annanicky90gmail.com>
2024-10-21 00:08:10 +0400

    create a notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const heading = document.createElement('h2');
+  const paragraph = document.createElement('p');
+
+  notification.classList.add('notification', type);
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  heading.className = 'title';
+  heading.textContent = title;
+  paragraph.className = 'description';
+  paragraph.textContent = description;
+  paragraph.style.whiteSpace = 'pre-wrap';
+
+  notification.appendChild(heading);
+  notification.appendChild(paragraph);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
