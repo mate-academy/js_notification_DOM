2c1831a6ef6ee39dd0205e5e2227f47c03e6aa10
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2024-11-23 21:08:34 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,40 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  block.classList.add('notification');
+  h2.classList.add('title');
+
+  h2.textContent = title;
+  p.textContent = description;
+
+  block.style.top = `${posTop}px`;
+  block.style.right = `${posRight}px`;
+
+  block.append(h2);
+  block.append(p);
+  document.body.append(block);
+
+  const removeNotification = () => {
+    block.style.display = 'none';
+  };
+
+  switch (type) {
+    case 'success':
+      block.classList.add('success');
+      break;
+    case 'warning':
+      block.classList.add('warning');
+      break;
+    case 'error':
+      block.classList.add('error');
+      break;
+  }
+
+  setTimeout(removeNotification, 2000);
 };
 
 pushNotification(
