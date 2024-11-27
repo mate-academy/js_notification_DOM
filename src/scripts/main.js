2d944e9bed2cb464be053c111d5e7364cefad862
Andrii Tkachuk <kovklad@gmail.com>
2024-11-27 16:13:21 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  div.className = 'notification';
+  div.classList.add(type);
+  div.style.top = posTop + 'px';
+  div.style.right = posRight + 'px';
+  div.visibility = 'visible';
+  h2.textContent = title;
+  h2.className = 'title';
+  p.textContent = description;
+  div.append(h2, p);
+  body.append(div);
+
+  function hide(someDiv) {
+    someDiv.style.visibility = 'hidden';
+  }
+
+  setTimeout(() => hide(div), 2000);
 };
 
 pushNotification(
