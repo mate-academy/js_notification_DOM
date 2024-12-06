8825b6b5f64d12423599b21972543ba591e843b1
Daniel-spec-dell <fvgdanil@gmail.com>
2024-12-06 06:16:00 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,37 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  // Створити контейнер div для повідомлень.
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+  notification.style.position = 'absolute';
+  notification.style.right = `${posRight}px`;
+  notification.style.top = `${posTop}px`;
+
+  // Створити заголовок
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.className = 'title';
+  notificationTitle.textContent = title;
+
+  // Створити опис
+  const notificationDescription = document.createElement('p');
+
+  notificationDescription.textContent = description;
+
+  // Вставити заголовок і опис в контейнер повідомлення
+  notification.appendChild(notificationTitle);
+  notification.appendChild(notificationDescription);
+
+  // Вставити повідомлення в body
+  document.body.appendChild(notification);
+
+  // Видалити повідомлення через 2 секунди
+
+  setTimeout(() => {
+    document.body.removeChild(notification);
+  }, 2000);
 };
 
 pushNotification(
