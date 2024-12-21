c1e4f0bbf4f28912607819147d50c33b82a9097d
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-21 17:36:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  div.classList.toggle('notification');
+  div.classList.toggle(type);
+  div.scrollTop = posTop;
+  div.scrollLeft = posRight;
+  h2.classList.toggle('title');
+  h2.textContent = title;
+
+  p.textContent = description;
+
+  document.body.appendChild(div);
+  div.prepend(h2);
+  div.append(p);
+
+  setTimeout(() => {
+    div.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
