5c884c717ea174251909afdac0d4f3c3ab00bbda
Nikolay <diedaemon@gmail.com>
2025-01-08 17:37:20 +0100

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.querySelector('body');
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+
+  div.classList.add('notification', type);
+  div.style.top = posTop + 'px';
+  div.style.right = posRight + 'px';
+
+  h2.classList.add('title');
+  h2.textContent = title;
+
+  p.textContent = description;
+
+  div.appendChild(h2);
+  div.appendChild(p);
+
+  body.appendChild(div);
 };
 
 pushNotification(
