6cbb3cb48cb33045be0493dddea4c4f0356d41da
Artem <skickoartem74@gmail.com>
2025-01-30 14:05:50 +0100

    final Soullution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,44 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  setTimeout(() => {
+    // notification
+    const notification = document.createElement('div');
+
+    notification.setAttribute('class', type);
+
+    if (type === 'success') {
+      notification.style.backgroundColor = '#66FF99';
+    } else if (type === 'error') {
+      notification.style.backgroundColor = '#ffcccb';
+    } else if (type === 'warning') {
+      notification.style.backgroundColor = '#FFFFE0';
+    }
+
+    // title
+    const mainTitle = document.createElement('h2');
+
+    mainTitle.textContent = title;
+    mainTitle.setAttribute('class', 'title');
+
+    // description
+    const descriptionText = document.createElement('p');
+
+    descriptionText.textContent = description;
+    descriptionText.style.whiteSpace = 'pre-line';
+
+    // styles
+    notification.style.position = 'absolute';
+    notification.style.top = posTop + 'px';
+    notification.style.right = posRight + 'px';
+    notification.style.borderRadius = '10px';
+    notification.style.marginBottom = '20px';
+
+    // appending childs
+    notification.appendChild(mainTitle);
+    notification.appendChild(descriptionText);
+    document.body.appendChild(notification);
+  }, 2.0 * 1000);
 };
 
 pushNotification(
