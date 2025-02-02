6e2bb33d0299f65b273929d03b4ae23014cf3c9a
Alina <alina220380@gmail.com>
2025-02-02 12:16:38 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,30 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notification = document.createElement('div');
+
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  notification.classList.add('notification', type);
+
+  const text1 = document.createElement('h2');
+
+  text1.classList.add('title');
+  text1.textContent = title;
+
+  const text2 = document.createElement('p');
+
+  text2.textContent = description;
+
+  notification.appendChild(text1);
+  notification.appendChild(text2);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.visibility = 'hidden'; // Робимо невидимим
+  }, 2000);
 };
 
 pushNotification(
