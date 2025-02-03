4eb44e37e74d73697b8c7eb0c9b6a479c9954029
Olha Lishenko <lishenko.olha@gmail.com>
2025-02-03 13:37:29 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,44 @@
 'use strict';
 
+const notifBlock = document.createElement('div');
+
+notifBlock.setAttribute('class', 'notification-block');
+notifBlock.style.top = '10px';
+notifBlock.style.right = '10px';
+notifBlock.style.position = 'fixed';
+notifBlock.style.display = 'flex';
+notifBlock.style.flexDirection = 'column';
+notifBlock.style.gap = '10px';
+notifBlock.style.opacity = '1';
+notifBlock.style.transition = 'opacity 1s ease-out';
+
+document.body.appendChild(notifBlock);
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  const notificationTitle = document.createElement('h2');
+  const notificationDescrip = document.createElement('p');
+
+  notification.setAttribute('class', 'notification');
+  notification.classList.add(type);
+
+  notification.style.position = 'static';
+
+  notificationTitle.setAttribute('class', 'title');
+  notificationTitle.textContent = title;
+  notificationDescrip.setAttribute('class', 'description');
+  notificationDescrip.textContent = description;
+
+  notification.appendChild(notificationTitle);
+  notificationTitle.insertAdjacentElement('afterend', notificationDescrip);
+
+  notifBlock.appendChild(notification);
 };
+
+setTimeout(() => {
+  notifBlock.style.display = 'none';
+}, 2000);
 
 pushNotification(
   10,
