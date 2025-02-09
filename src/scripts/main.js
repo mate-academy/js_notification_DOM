f43bcc0a581e6d11ee5ce09fc4ac18bac0b00ef1
Vorona <voron1765@gmail.com>
2025-02-09 13:25:02 +0200

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const notificationTitle = document.createElement('h2');
+  const notificationDescr = document.createElement('p');
+
+  notification.classList.add('notification', type);
+  notificationTitle.classList.add('title');
+
+  notificationTitle.textContent = title;
+  notificationDescr.textContent = description;
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  notification.append(notificationTitle, notificationDescr);
+  document.body.appendChild(notification);
 };
 
 pushNotification(
   10,
   10,
   'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.\nNotification should contain title and description.',
   'success',
 );
 
@@ -16,7 +30,7 @@ pushNotification(
   150,
   10,
   'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.\nNotification should contain title and description.',
   'error',
 );
 
@@ -24,6 +38,6 @@ pushNotification(
   290,
   10,
   'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.\nNotification should contain title and description.',
   'warning',
 );
