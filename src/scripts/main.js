d343a5f24a8bd65faa0db1278483d9400efadbbf
anton1dev <antek.ukraine@gmail.com>
2025-02-12 14:43:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notificationElement = document.createElement('div');
+
+  notificationElement.classList.add('notification', type);
+  notificationElement.style.top = posTop + 'px';
+  notificationElement.style.right = posRight + 'px';
+
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.classList.add('title');
+  notificationTitle.textContent = title;
+
+  const notificationDescription = document.createElement('p');
+
+  notificationDescription.textContent = description;
+
+  document.body.appendChild(notificationElement);
+  notificationElement.appendChild(notificationTitle);
+  notificationElement.appendChild(notificationDescription);
+
+  setTimeout(() => {
+    notificationElement.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
