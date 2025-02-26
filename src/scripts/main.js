a5b06c7e827847de161e3bb139fd37322e03e8e7
Anna Poplavska <annapoplavska10@gmail.com>
2025-02-26 11:03:11 +0100

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const box = document.createElement('div');
+  const header = document.createElement('h2');
+  const text = document.createElement('p');
+
+  box.setAttribute('class', `notification ${type}`);
+  box.style.top = `${posTop}px`;
+  box.style.right = `${posRight}px`;
+  header.setAttribute('class', 'title');
+  header.textContent = title;
+  text.textContent = description;
+
+  setTimeout(() => {
+    box.style.display = 'none';
+  }, 2000);
+
+  box.appendChild(header);
+  box.appendChild(text);
+  document.querySelector('body').appendChild(box);
 };
 
 pushNotification(
