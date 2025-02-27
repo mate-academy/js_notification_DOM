423cba3fae179157035d1e105dd09dd15e710aad
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-02-27 13:34:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div');
+  const titleMessage = document.createElement('h2');
+  const descr = document.createElement('p');
+
+  message.style.top = posTop + 'px';
+  message.style.right = posRight + 'px';
+  titleMessage.textContent = title;
+  descr.textContent = description;
+  message.append(titleMessage, descr);
+  message.classList.add('notification', type);
+  titleMessage.classList.add('title');
+  document.body.append(message);
+
+  return message;
 };
 
-pushNotification(
+const message1 = pushNotification(
   10,
   10,
   'Title of Success message',
@@ -12,7 +25,7 @@ pushNotification(
   'success',
 );
 
-pushNotification(
+const message2 = pushNotification(
   150,
   10,
   'Title of Error message',
@@ -20,10 +33,14 @@ pushNotification(
   'error',
 );
 
-pushNotification(
+const message3 = pushNotification(
   290,
   10,
   'Title of Warning message',
   'Message example.\n ' + 'Notification should contain title and description.',
   'warning',
 );
+
+setTimeout(() => (message1.style.display = 'none'), 2000);
+setTimeout(() => (message2.style.display = 'none'), 2000);
+setTimeout(() => (message3.style.display = 'none'), 2000);
