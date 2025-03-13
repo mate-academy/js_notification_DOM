25e11008d50c66ddc1fea0d3022302a245a687ee
Lera <valeriamelesenko4@gmail.com>
2025-03-13 21:27:21 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,32 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const element = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const body = document.querySelector('body');
+  // eslint-disable-next-line no-undef
+  const p = document.createElement('p');
+
+  element.classList.add('notification', type);
+
+  element.style.top = `${posTop}px`;
+  element.style.right = `${posRight}px`;
+
+  h2.textContent = title;
+  h2.classList.add('title');
+
+  p.textContent = description;
+
+  element.appendChild(h2);
+  element.appendChild(p);
+  // eslint-disable-next-line no-console
+  console.log(element);
+
+  body.appendChild(element);
+
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
