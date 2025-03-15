547fc0a1349ac02ee8fda0eced7912b31c2bb49a
Anna Onyshchenko <auonishchenko.com>
2025-03-15 23:48:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,37 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.className = `notification ${type}`;
+  notification.style.position = 'absolute';
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+  notification.style.padding = '10px';
+  notification.style.borderRadius = '5px';
+  notification.style.boxShadow = '0px 0px 5px rgba(0, 0, 0, 0.2)';
+  notification.style.width = '250px';
+
+  // eslint-disable-next-line max-len
+  notification.style.backgroundColor =
+    type === 'success' ? 'green' : type === 'error' ? 'red' : 'orange';
+  notification.style.color = '#fff';
+
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.className = 'title';
+  notificationTitle.textContent = title;
+
+  const notificationDescription = document.createElement('p');
+
+  notificationDescription.textContent = description;
+
+  notification.appendChild(notificationTitle);
+  notification.appendChild(notificationDescription);
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => notification.remove(), 2000);
 };
 
 pushNotification(
