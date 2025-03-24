3caff9fbc2a01f80b47dbc0803875c3799316311
Karina Turchyna <kari.tu99@gmail.com>
2025-03-24 16:46:35 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,28 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const newNotification = document.createElement('div');
+  const descriptionNotification = document.createElement('p');
+
+  descriptionNotification.textContent = description;
+
+  const titleNotification = document.createElement('h2');
+
+  titleNotification.textContent = title;
+  titleNotification.classList.add('title');
+
+  newNotification.append(titleNotification, descriptionNotification);
+
+  newNotification.classList.add('notification', type);
+
+  newNotification.style.top = posTop + 'px';
+  newNotification.style.right = posRight + 'px';
+
+  document.body.appendChild(newNotification);
+
+  setTimeout(() => {
+    newNotification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
