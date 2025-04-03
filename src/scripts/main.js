ab46990e560e4e1bc6f2413850ea8ffe23fdf1f1
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-03 22:05:25 +0300

    add pushNotification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,46 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  let classList = '';
+
+  switch (type) {
+    case 'success':
+      classList = 'notification success';
+      break;
+
+    case 'error':
+      classList = 'notification error';
+      break;
+
+    case 'warning':
+      classList = 'notification warning';
+      break;
+
+    default:
+      break;
+  }
+
+  const notification = document.createElement('div');
+  const tagTitle = document.createElement('h2');
+  const tagDescription = document.createElement('p');
+
+  tagTitle.textContent = title;
+  tagTitle.classList = 'title';
+  tagDescription.textContent = description;
+
+  notification.classList = classList;
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  notification.appendChild(tagTitle);
+  notification.appendChild(tagDescription);
+  body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
