4f82c34f74a8bfcc118528bb022a11eaca7e392a
Galyna Maikovych <gmaik.com@gmail.com>
2025-04-05 17:40:04 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,36 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const message = document.createElement('div');
+
+  message.classList.add('notification');
+  message.classList.add(type);
+
+  const messageTitle = document.createElement('h2');
+  const text = document.createElement('p');
+
+  messageTitle.classList.add('title');
+  messageTitle.textContent = title;
+  text.textContent = description;
+
+  message.append(messageTitle);
+  message.append(text);
+
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  document.body.appendChild(message);
+
+  setTimeout(() => {
+    message.style.opacity = '0';
+    message.style.transition = 'opacity 0.3s';
+
+    setTimeout(() => {
+      if (message.parentElement) {
+        message.parentElement.removeChild(message);
+      }
+    }, 300); // wait for fade-out to complete
+  }, 2000);
 };
 
 pushNotification(
