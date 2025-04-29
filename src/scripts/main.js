f042c037c56739b8f44533fb1096bd1d1d8505c8
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-29 07:39:14 +0100

    Notification DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,40 @@
 'use strict';
 
+const body = document.querySelector('body');
+
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notificationCard = document.createElement('div');
+
+  notificationCard.classList.add('notification');
+
+  notificationCard.style.top = `${posTop}px`;
+  notificationCard.style.right = `${posRight}px`;
+
+  const notificationTitle = document.createElement('h2');
+
+  notificationTitle.classList.add('title');
+  notificationTitle.textContent = title;
+
+  const notificationInfo = document.createElement('p');
+
+  notificationInfo.innerText = description;
+
+  notificationCard.appendChild(notificationTitle);
+  notificationCard.appendChild(notificationInfo);
+
+  body.appendChild(notificationCard);
+
+  if (type === 'success') {
+    notificationCard.classList.add('success');
+  } else if (type === 'error') {
+    notificationCard.classList.add('error');
+  } else if (type === 'warning') {
+    notificationCard.classList.add('warning');
+  }
+
+  setTimeout(() => {
+    notificationCard.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
