5081a596b7994455e87483362c03b1c7f51dee9a
Olena Tatyuk <tatyukolena@gmail.com>
2025-05-16 14:20:08 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,7 @@ const pushNotification = (posTop, posRight, title, description, type) => {
   document.body.appendChild(notification);
 
   setTimeout(() => {
-    notification.style.display = 'none';
+    notification.remove();
   }, 2000);
 };
 
