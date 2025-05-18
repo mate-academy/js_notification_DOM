fba12c934cdc02dd0bc52223f419ff845d63a4fc
Nazar Bakay <bakayn484@gmail.com>
2025-05-19 00:11:15 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,27 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const elementDiv = document.createElement('div');
+
+  elementDiv.classList.add('notification', type);
+  elementDiv.style.top = posTop + 'px';
+  elementDiv.style.right = posRight + 'px';
+
+  const header = document.createElement('h2');
+
+  header.classList.add('title');
+  header.textContent = title;
+
+  const paragraph = document.createElement('p');
+
+  paragraph.textContent = description;
+
+  document.body.appendChild(elementDiv);
+  elementDiv.append(header, paragraph);
+
+  setTimeout(() => {
+    elementDiv.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
