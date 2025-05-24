07caba83fd0566f4c73d6769cb6b14b55d10ec88
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-24 20:21:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const ntfTitle = document.createElement('h2');
+  const ntfDescription = document.createElement('p');
+
+  notification.classList.add('notification', type);
+  ntfTitle.classList.add('title');
+
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  ntfTitle.textContent = title;
+  ntfDescription.textContent = description;
+
+  notification.append(ntfTitle, ntfDescription);
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.opacity = 0;
+  }, 2000);
 };
 
 pushNotification(
@@ -13,7 +31,7 @@ pushNotification(
 );
 
 pushNotification(
-  150,
+  160,
   10,
   'Title of Error message',
   'Message example.\n ' + 'Notification should contain title and description.',
