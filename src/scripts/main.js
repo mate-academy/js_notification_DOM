f6ced22ac12736629625799fb261a3cd14a5ca12
Ilona-Tkachenko <ilonatkachenko796@gmail.com>
2025-05-31 13:16:06 +0300

    final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,31 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+
+  const heading = document.createElement('h2');
+
+  heading.classList.add('title');
+  heading.textContent = title;
+
+  const paragraph = document.createElement('p');
+
+  paragraph.textContent = description;
+
+  notification.appendChild(heading);
+  notification.appendChild(paragraph);
+
+  notification.style.position = 'absolute';
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
