2f539d84258576334f00c6193af88108341ffeed
Viktor Kyshenchuk <viktor.kyshenchuk@gmail.com>
2025-06-03 23:36:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const element = document.createElement('div');
+  const titleEl = document.createElement('h2');
+  const paragraphEl = document.createElement('p');
+
+  element.classList.add('notification', type);
+  titleEl.classList.add('title');
+
+  titleEl.textContent = title;
+  paragraphEl.textContent = description;
+
+  element.style.cssText = `position: absolute; top: ${posTop}px; right: ${posRight}px;`;
+
+  element.appendChild(titleEl);
+  element.appendChild(paragraphEl);
+  document.body.appendChild(element);
+
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
