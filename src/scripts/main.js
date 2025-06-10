51c681dc89ecfe21a2f3e6f6a761d4b99f91ae79
Daria <darialys2510@gmail.com>
2025-06-10 16:36:11 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,29 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const block = document.createElement('div');
+
+  block.classList.add('notification');
+  block.classList.add(type);
+  block.style.top = `${posTop}px`;
+  block.style.right = `${posRight}px`;
+
+  const blockTitle = document.createElement('h2');
+
+  blockTitle.classList.add('title');
+  blockTitle.textContent = title;
+
+  const blockDecription = document.createElement('p');
+
+  blockDecription.textContent = description;
+
+  document.body.appendChild(block);
+  block.appendChild(blockTitle);
+  block.appendChild(blockDecription);
+
+  setTimeout(() => {
+    block.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
