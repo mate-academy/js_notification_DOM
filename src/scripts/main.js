133a3c106d47a419702a0738908158aa39e01b08
Yuriy Postrygan <petrikeich@gmail.com>
2025-07-04 17:51:23 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,37 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+  const body = document.querySelector('body');
+
+  body.insertAdjacentElement('beforeend', notification);
+
+  const h2 = document.createElement('h2');
+
+  notification.insertAdjacentElement('afterbegin', h2);
+
+  notification.insertAdjacentHTML('beforeend', `<p>${description}</p>`);
+  h2.insertAdjacentHTML('afterbegin', `${title}`);
+
+  notification.setAttribute('class', 'notification');
+
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
+
+  notification.classList.add(type);
+  h2.classList.add('title');
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
   10,
   10,
   'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.<br> ' +
+    'Notification should contain title and description.',
   'success',
 );
 
@@ -16,7 +39,8 @@ pushNotification(
   150,
   10,
   'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.<br> ' +
+    'Notification should contain title and description.',
   'error',
 );
 
@@ -24,6 +48,7 @@ pushNotification(
   290,
   10,
   'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example.<br> ' +
+    'Notification should contain title and description.',
   'warning',
 );
